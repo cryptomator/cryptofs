a75fa3e53a3b37ff83430154dad9c02a817488ff
Markus Kreusch <mail@markuskreusch.de>
2016-08-15 15:02:59 +0200

    Fixed codacy issue regarding field order

--- src/main/java/org/cryptomator/cryptofs/ChunkData.java
+++ src/main/java/org/cryptomator/cryptofs/ChunkData.java
@@ -3,14 +3,6 @@ package org.cryptomator.cryptofs;
 import java.nio.ByteBuffer;
 
 class ChunkData {
-
-	public static ChunkData writtenChunkData(ByteBuffer bytes) {
-		return new ChunkData(bytes, true);
-	}
-
-	public static ChunkData readChunkData(ByteBuffer bytes) {
-		return new ChunkData(bytes, false);
-	}
 	
 	private final ByteBuffer bytes;
 	private final boolean written;
@@ -18,6 +10,14 @@ class ChunkData {
 	private ChunkData(ByteBuffer bytes, boolean written) {
 		this.bytes = bytes;
 		this.written = written;
+	}
+
+	public static ChunkData writtenChunkData(ByteBuffer bytes) {
+		return new ChunkData(bytes, true);
+	}
+
+	public static ChunkData readChunkData(ByteBuffer bytes) {
+		return new ChunkData(bytes, false);
 	}
 	
 	public ByteBuffer bytes() {
