7eb4076b889993282ed3e5ec8d5ed4460d3d6b0e
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2016-09-03 20:46:03 +0200

    simplification [ci skip]

--- src/test/java/org/cryptomator/cryptofs/CryptoFileSystemTest.java
+++ src/test/java/org/cryptomator/cryptofs/CryptoFileSystemTest.java
@@ -9,11 +9,8 @@
 package org.cryptomator.cryptofs;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.concurrent.ConcurrentHashMap;
@@ -53,21 +50,7 @@ public class CryptoFileSystemTest {
 
 	@After
 	public void teardown() throws IOException {
-		Files.walkFileTree(tmpPath, new SimpleFileVisitor<Path>() {
-
-			@Override
-			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-				Files.deleteIfExists(file);
-				return FileVisitResult.CONTINUE;
-			}
-
-			@Override
-			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-				Files.deleteIfExists(dir);
-				return FileVisitResult.CONTINUE;
-			}
-
-		});
+		Files.walkFileTree(tmpPath, new DeletingFileVisitor());
 	}
 
 	@Test
