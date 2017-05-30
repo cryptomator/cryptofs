2387356e33acdfa2d5ea29188bf9e888719fa19e
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2017-05-30 11:47:36 +0200

    Single I/O operation in DirectoryIdLoader

--- src/main/java/org/cryptomator/cryptofs/DirectoryIdLoader.java
+++ src/main/java/org/cryptomator/cryptofs/DirectoryIdLoader.java
@@ -1,9 +1,12 @@
 package org.cryptomator.cryptofs;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.UUID;
 
 import javax.inject.Inject;
@@ -13,19 +16,29 @@ import com.google.common.cache.CacheLoader;
 @PerFileSystem
 class DirectoryIdLoader extends CacheLoader<Path, String> {
 
+	private static final int MAX_DIR_ID_LENGTH = 1000;
+
 	@Inject
 	public DirectoryIdLoader() {
 	}
 
 	@Override
 	public String load(Path dirFilePath) throws IOException {
-		if (Files.exists(dirFilePath)) {
-			byte[] bytes = Files.readAllBytes(dirFilePath);
-			if (bytes.length == 0) {
+		try (FileChannel ch = FileChannel.open(dirFilePath, StandardOpenOption.READ)) {
+			long size = ch.size();
+			if (size == 0) {
 				throw new IOException("Invalid, empty directory file: " + dirFilePath);
+			} else if (size > MAX_DIR_ID_LENGTH) {
+				throw new IOException("Unexpectedly large directory file: " + dirFilePath);
+			} else {
+				assert size <= MAX_DIR_ID_LENGTH; // thus int
+				ByteBuffer buffer = ByteBuffer.allocate((int) size);
+				int read = ch.read(buffer);
+				assert read == size;
+				buffer.flip();
+				return StandardCharsets.UTF_8.decode(buffer).toString();
 			}
-			return new String(bytes, StandardCharsets.UTF_8);
-		} else {
+		} catch (NoSuchFileException e) {
 			return UUID.randomUUID().toString();
 		}
 	}
