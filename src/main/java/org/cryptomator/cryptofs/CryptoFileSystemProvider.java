dbf17cae20c6079211c12691caa70cf536dc88ce
Markus Kreusch <mail@markuskreusch.de>
2017-10-27 22:18:43 +0200

    Refactoring: Split up method

--- src/main/java/org/cryptomator/cryptofs/CryptoFileSystemProvider.java
+++ src/main/java/org/cryptomator/cryptofs/CryptoFileSystemProvider.java
@@ -254,15 +254,25 @@ public class CryptoFileSystemProvider extends FileSystemProvider {
 		CryptoFileSystemUri parsedUri = CryptoFileSystemUri.parse(uri);
 		CryptoFileSystemProperties properties = CryptoFileSystemProperties.wrap(rawProperties);
 
+		// TODO remove implicit initialization in 2.0.0
+		initializeFileSystemIfRequired(parsedUri, properties);
+		migrateFileSystemIfRequired(parsedUri, properties);
+
+		return fileSystems.create(parsedUri.pathToVault(), properties);
+	}
+
+	// TODO remove implicit initialization in 2.0.0
+	private void initializeFileSystemIfRequired(CryptoFileSystemUri parsedUri, CryptoFileSystemProperties properties) throws NotDirectoryException, IOException, NoSuchFileException {
 		if (!CryptoFileSystemProvider.containsVault(parsedUri.pathToVault(), properties.masterkeyFilename())) {
-			// TODO remove implicit initialization in 2.0.0:
 			if (properties.initializeImplicitly()) {
 				CryptoFileSystemProvider.initialize(parsedUri.pathToVault(), properties.masterkeyFilename(), properties.passphrase());
 			} else {
 				throw new NoSuchFileException(parsedUri.pathToVault().toString(), null, "Vault not initialized.");
 			}
 		}
+	}
 
+	private void migrateFileSystemIfRequired(CryptoFileSystemUri parsedUri, CryptoFileSystemProperties properties) throws IOException, FileSystemNeedsMigrationException {
 		if (Migrators.get().needsMigration(parsedUri.pathToVault(), properties.masterkeyFilename())) {
 			if (properties.migrateImplicitly()) {
 				Migrators.get().migrate(parsedUri.pathToVault(), properties.masterkeyFilename(), properties.passphrase());
@@ -270,8 +280,6 @@ public class CryptoFileSystemProvider extends FileSystemProvider {
 				throw new FileSystemNeedsMigrationException(parsedUri.pathToVault());
 			}
 		}
-
-		return fileSystems.create(parsedUri.pathToVault(), properties);
 	}
 
 	@Override
