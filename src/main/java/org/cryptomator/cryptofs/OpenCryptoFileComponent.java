491bf6f0c1f383246435ee81d7339f1634043fb5
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2018-11-14 14:16:11 +0100

    merged redundant dagger modules

--- src/main/java/org/cryptomator/cryptofs/OpenCryptoFileComponent.java
+++ src/main/java/org/cryptomator/cryptofs/OpenCryptoFileComponent.java
@@ -2,7 +2,7 @@ package org.cryptomator.cryptofs;
 
 import dagger.Subcomponent;
 
-@Subcomponent(modules = {OpenCryptoFileModule.class, OpenCryptoFileFactoryModule.class})
+@Subcomponent(modules = {OpenCryptoFileModule.class})
 @PerOpenFile
 interface OpenCryptoFileComponent {
 
--- src/main/java/org/cryptomator/cryptofs/OpenCryptoFileModule.java
+++ src/main/java/org/cryptomator/cryptofs/OpenCryptoFileModule.java
@@ -1,9 +1,19 @@
 package org.cryptomator.cryptofs;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.util.concurrent.atomic.AtomicLong;
 
 import dagger.Module;
 import dagger.Provides;
+import org.cryptomator.cryptolib.api.Cryptor;
+import org.cryptomator.cryptolib.api.FileHeader;
+
+import static org.cryptomator.cryptofs.UncheckedThrows.rethrowUnchecked;
+import static org.cryptomator.cryptolib.Cryptors.cleartextSize;
 
 @Module
 class OpenCryptoFileModule {
@@ -27,6 +37,61 @@ class OpenCryptoFileModule {
 	@PerOpenFile
 	public EffectiveOpenOptions provideOptions() {
 		return options;
+	}
+
+	@Provides
+	@PerOpenFile
+	public FileChannel provideFileChannel(@OriginalOpenFilePath Path path, EffectiveOpenOptions options) {
+		return rethrowUnchecked(IOException.class).from(() -> path.getFileSystem().provider().newFileChannel(path, options.createOpenOptionsForEncryptedFile()));
+	}
+
+	@Provides
+	@PerOpenFile
+	public BasicFileAttributeView provideBasicFileAttributeView(@OriginalOpenFilePath Path path) {
+		return path.getFileSystem().provider().getFileAttributeView(path, BasicFileAttributeView.class);
+	}
+
+	@Provides
+	@PerOpenFile
+	@OpenFileSize
+	public AtomicLong provideFileSize(FileChannel channel, Cryptor cryptor) {
+		return rethrowUnchecked(IOException.class).from(() -> {
+			long size = channel.size();
+			if (size == 0) {
+				return new AtomicLong();
+			} else {
+				int headerSize = cryptor.fileHeaderCryptor().headerSize();
+				return new AtomicLong(cleartextSize(size - headerSize, cryptor));
+			}
+		});
+	}
+
+	@Provides
+	@PerOpenFile
+	public FileHeader provideFileHeader(FileChannel channel, Cryptor cryptor, EffectiveOpenOptions options) {
+		return rethrowUnchecked(IOException.class).from(() -> {
+			if (options.truncateExisting() || isNewFile(channel, options)) {
+				FileHeader newHeader = cryptor.fileHeaderCryptor().create();
+				channel.position(0);
+				channel.write(cryptor.fileHeaderCryptor().encryptHeader(newHeader));
+				channel.force(false);
+				return newHeader;
+			} else {
+				ByteBuffer existingHeaderBuf = ByteBuffer.allocate(cryptor.fileHeaderCryptor().headerSize());
+				channel.position(0);
+				channel.read(existingHeaderBuf);
+				existingHeaderBuf.flip();
+				try {
+					return cryptor.fileHeaderCryptor().decryptHeader(existingHeaderBuf);
+				} catch (IllegalArgumentException e) {
+					throw new IOException(e);
+				}
+			}
+		});
+	}
+
+	private boolean isNewFile(FileChannel channel, EffectiveOpenOptions options) throws IOException {
+		return options.createNew() || options.create() && channel.size() == 0;
 	}
 
 	public static Builder openCryptoFileModule() {
