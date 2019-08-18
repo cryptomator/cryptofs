9339075967dfb72cc268cd4e2a8a3e75373a4c0f
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2019-08-18 13:53:16 +0200

    Revert 3ce8537eb84f887d608c3e2d9396308898bccb71 due to incompatibilities.
    We need to defer #60 and introduce a new vault format

--- src/main/java/org/cryptomator/cryptofs/Constants.java
+++ src/main/java/org/cryptomator/cryptofs/Constants.java
@@ -15,7 +15,7 @@ public final class Constants {
 
 	static final String DATA_DIR_NAME = "d";
 	static final String METADATA_DIR_NAME = "m";
-	static final int SHORT_NAMES_MAX_LENGTH = 254; // length of a ciphertext filename before it gets shortened. It should be less than 260 (Windows MAX_PATH limit) and a multiple of 8 when the file extensions (4) and prefix (2) is subtracted.
+	static final int SHORT_NAMES_MAX_LENGTH = 129;
 	static final String ROOT_DIR_ID = "";
 
 	static final int MAX_SYMLINK_LENGTH = 32767; // max path length on NTFS and FAT32: 32k-1
--- src/test/java/org/cryptomator/cryptofs/ConflictResolverTest.java
+++ src/test/java/org/cryptomator/cryptofs/ConflictResolverTest.java
@@ -1,6 +1,5 @@
 package org.cryptomator.cryptofs;
 
-import com.google.common.base.Strings;
 import org.cryptomator.cryptolib.api.AuthenticationFailedException;
 import org.cryptomator.cryptolib.api.Cryptor;
 import org.cryptomator.cryptolib.api.FileNameCryptor;
@@ -131,8 +130,7 @@ public class ConflictResolverTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"ABCDEF== (1).lng", "conflict_ABCDEF==.lng"})
 	public void testRenameLongFile(String conflictingFileName) throws IOException {
-		String longCiphertextName = Strings.repeat("ABCDEFGH",Constants.SHORT_NAMES_MAX_LENGTH /8 + 1);
-
+		String longCiphertextName = "ABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGHABCDEFGH2345====";
 		assert longCiphertextName.length() > Constants.SHORT_NAMES_MAX_LENGTH;
 		Mockito.when(testFileName.toString()).thenReturn(conflictingFileName);
 		Mockito.when(longFileNameProvider.inflate("ABCDEF==.lng")).thenReturn("FEDCBA==");
