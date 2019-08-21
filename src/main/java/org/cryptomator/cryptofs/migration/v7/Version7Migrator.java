93273e8f47476a0eed08df5c07a35f197a9c8342
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2019-08-21 18:01:44 +0200

    removed unused code

--- src/main/java/org/cryptomator/cryptofs/migration/v7/Version7Migrator.java
+++ src/main/java/org/cryptomator/cryptofs/migration/v7/Version7Migrator.java
@@ -5,7 +5,6 @@
  *******************************************************************************/
 package org.cryptomator.cryptofs.migration.v7;
 
-import com.google.common.io.BaseEncoding;
 import org.cryptomator.cryptofs.BackupUtil;
 import org.cryptomator.cryptofs.Constants;
 import org.cryptomator.cryptofs.migration.api.Migrator;
@@ -14,14 +13,12 @@ import org.cryptomator.cryptolib.api.CryptorProvider;
 import org.cryptomator.cryptolib.api.InvalidPassphraseException;
 import org.cryptomator.cryptolib.api.KeyFile;
 import org.cryptomator.cryptolib.api.UnsupportedVaultFormatException;
-import org.cryptomator.cryptolib.common.MessageDigestSupplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.SeekableByteChannel;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileVisitOption;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -31,19 +28,11 @@ import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Map;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
+import java.util.Optional;
 
 public class Version7Migrator implements Migrator {
 
 	private static final Logger LOG = LoggerFactory.getLogger(Version7Migrator.class);
-
-	private static final BaseEncoding BASE32 = BaseEncoding.base32();
-	private static final int FILENAME_BUFFER_SIZE = 10 * 1024;
-	private static final String NEW_SHORTENED_SUFFIX = ".c9s";
-	private static final String NEW_NORMAL_SUFFIX = ".c9r";
 
 	private final CryptorProvider cryptorProvider;
 
@@ -64,8 +53,7 @@ public class Version7Migrator implements Migrator {
 			Files.copy(masterkeyFile, masterkeyBackupFile, StandardCopyOption.REPLACE_EXISTING);
 			LOG.info("Backed up masterkey from {} to {}.", masterkeyFile.getFileName(), masterkeyBackupFile.getFileName());
 
-			Map<String, String> namePairs = loadShortenedNames(vaultRoot);
-			migrateFileNames(namePairs, vaultRoot);
+			migrateFileNames(vaultRoot);
 
 			// TODO remove deprecated .lng from /m/
 
@@ -77,76 +65,31 @@ public class Version7Migrator implements Migrator {
 		LOG.info("Upgraded {} from version 6 to version 7.", vaultRoot);
 	}
 
-	/**
-	 * With vault format 7 we increased the file shortening threshold.
-	 *
-	 * @param vaultRoot
-	 * @return
-	 * @throws IOException
-	 */
-	// visible for testing
-	Map<String, String> loadShortenedNames(Path vaultRoot) throws IOException {
-		Path metadataDir = vaultRoot.resolve("m");
-		Map<String, String> result = new HashMap<>();
-		ByteBuffer longNameBuffer = ByteBuffer.allocate(FILENAME_BUFFER_SIZE);
-		Files.walkFileTree(metadataDir, EnumSet.noneOf(FileVisitOption.class), 3, new SimpleFileVisitor<Path>() {
-
-			@Override
-			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-				try (SeekableByteChannel ch = Files.newByteChannel(file, StandardOpenOption.READ)) {
-					if (ch.size() > longNameBuffer.capacity()) {
-						LOG.error("Migrator not suited to handle filenames as large as {}. Aborting without changes.", ch.size());
-						throw new IOException("Filename too large for migration: " + file);
-					} else {
-						longNameBuffer.clear();
-						ch.read(longNameBuffer);
-						longNameBuffer.flip();
-						String longName = UTF_8.decode(longNameBuffer).toString();
-						String shortName = file.getFileName().toString();
-						result.put(shortName, longName);
-						return FileVisitResult.CONTINUE;
-					}
-				}
-			}
-		});
-		return result;
-	}
-
-	void migrateFileNames(Map<String, String> deflatedNames, Path vaultRoot) throws IOException {
+	private void migrateFileNames(Path vaultRoot) throws IOException {
 		Path dataDir = vaultRoot.resolve("d");
 		Files.walkFileTree(dataDir, EnumSet.noneOf(FileVisitOption.class), 3, new SimpleFileVisitor<Path>() {
 
 			@Override
-			public FileVisitResult visitFile(Path oldfile, BasicFileAttributes attrs) throws IOException {
-				String oldfilename = oldfile.getFileName().toString();
-				String newfilename;
-				if (deflatedNames.containsKey(oldfilename)) {
-					newfilename = deflatedNames.get(oldfilename) + NEW_NORMAL_SUFFIX;
-				} else {
-					newfilename = oldfilename + NEW_NORMAL_SUFFIX;
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				final Optional<FilePathMigration> migration;
+				try {
+					migration = FilePathMigration.parse(vaultRoot, file);
+				} catch (UninflatableFileException e) {
+					LOG.warn("SKIP {} because inflation failed.", file);
+					return FileVisitResult.CONTINUE;
 				}
-
-				if (newfilename.length() > 254) { // Value of Constants#SHORT_NAMES_MAX_LENGTH as in Vault Format 7
-					newfilename = deflate(vaultRoot, newfilename);
+				if (migration.isPresent()) {
+					try {
+						Path migratedFile = migration.get().migrate();
+						LOG.info("MOVED {} to {}", file, migratedFile);
+					} catch (FileAlreadyExistsException e) {
+						LOG.error("Failed to migrate " + file + " due to FileAlreadyExistsException. Already migrated?.", e);
+						return FileVisitResult.TERMINATE;
+					}
 				}
-
-				Path newfile = oldfile.resolveSibling(newfilename);
-				LOG.info("RENAME {} TO {}", oldfile, newfile);
 				return FileVisitResult.CONTINUE;
 			}
 		});
-	}
-
-	String deflate(Path vaultRoot, String longName) throws IOException {
-		Path metadataDir = vaultRoot.resolve("m");
-		byte[] longFileNameBytes = longName.getBytes(UTF_8);
-		byte[] hash = MessageDigestSupplier.SHA1.get().digest(longFileNameBytes);
-		String shortName = BASE32.encode(hash) + NEW_SHORTENED_SUFFIX;
-		Path metadataFile = metadataDir.resolve(shortName.substring(0, 2)).resolve(shortName.substring(2, 4)).resolve(shortName);
-		LOG.info("CREATE {}", metadataFile);
-		Files.createDirectories(metadataFile.getParent());
-		Files.write(metadataFile, shortName.getBytes(UTF_8));
-		return shortName;
 	}
 
 }
--- src/test/java/org/cryptomator/cryptofs/migration/v7/Version7MigratorTest.java
+++ src/test/java/org/cryptomator/cryptofs/migration/v7/Version7MigratorTest.java
@@ -1,6 +1,5 @@
 package org.cryptomator.cryptofs.migration.v7;
 
-import com.google.common.base.Strings;
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
 import org.cryptomator.cryptofs.migration.api.Migrator;
@@ -15,11 +14,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 
 public class Version7MigratorTest {
 
@@ -45,34 +42,6 @@ public class Version7MigratorTest {
 			KeyFile keyFile = cryptor.writeKeysToMasterkeyFile("test", 6);
 			Files.write(masterkeyFile, keyFile.serialize());
 		}
-		Path dataFile1 = dataDir.resolve("00/000000000000000000000000000000/111");
-		Path dataFile2 = dataDir.resolve("00/000000000000000000000000000000/1C6637A8F2E1F75E06FF9984894D6BD16A3A36A9.lng"); // 222
-		Path metaFile2 = metaDir.resolve("1C/66/1C6637A8F2E1F75E06FF9984894D6BD16A3A36A9.lng");
-		Path dataFile3 = dataDir.resolve("00/000000000000000000000000000000/0B51FC45F30A0C3027F2B4C4698C5EFCA3C62FE0.lng"); // 129 chars 33333...
-		Path metaFile3 = metaDir.resolve("0B/51/0B51FC45F30A0C3027F2B4C4698C5EFCA3C62FE0.lng");
-		Path dataFile4 = dataDir.resolve("00/000000000000000000000000000000/CAF8F7708CBF2FD3E735A0C765EBB8E0B879360A.lng"); // 130 chars 44444...
-		Path metaFile4 = metaDir.resolve("CA/F8/CAF8F7708CBF2FD3E735A0C765EBB8E0B879360A.lng");
-		Path dataFile5 = dataDir.resolve("00/000000000000000000000000000000/1CB1308D10CF786A827C91EEC1FF7B08D91ACAD6.lng"); // 250 chars 55555...
-		Path metaFile5 = metaDir.resolve("1C/B1/1CB1308D10CF786A827C91EEC1FF7B08D91ACAD6.lng");
-		Path dataFile6 = dataDir.resolve("00/000000000000000000000000000000/C7D6C6201B5344583A9ED2D8F5C3239CCF666230.lng"); // 251 chars 66666...
-		Path metaFile6 = metaDir.resolve("C7/D6/C7D6C6201B5344583A9ED2D8F5C3239CCF666230.lng");
-		Files.createDirectories(dataDir.resolve("00/000000000000000000000000000000"));
-		Files.createFile(dataFile1);
-		Files.createFile(dataFile2);
-		Files.createFile(dataFile3);
-		Files.createFile(dataFile4);
-		Files.createFile(dataFile5);
-		Files.createFile(dataFile6);
-		Files.createDirectories(metaFile2.getParent());
-		Files.createDirectories(metaFile3.getParent());
-		Files.createDirectories(metaFile4.getParent());
-		Files.createDirectories(metaFile5.getParent());
-		Files.createDirectories(metaFile6.getParent());
-		Files.write(metaFile2, Strings.repeat("2", 3).getBytes(StandardCharsets.UTF_8));
-		Files.write(metaFile3, Strings.repeat("3", 129).getBytes(StandardCharsets.UTF_8));
-		Files.write(metaFile4, Strings.repeat("4", 130).getBytes(StandardCharsets.UTF_8));
-		Files.write(metaFile5, Strings.repeat("5", 250).getBytes(StandardCharsets.UTF_8));
-		Files.write(metaFile6, Strings.repeat("6", 251).getBytes(StandardCharsets.UTF_8));
 	}
 
 	@AfterEach
@@ -81,21 +50,7 @@ public class Version7MigratorTest {
 	}
 
 	@Test
-	public void testLoadShortenedNames() throws IOException {
-		Version7Migrator migrator = new Version7Migrator(cryptorProvider);
-
-		Map<String, String> namePairs = migrator.loadShortenedNames(vaultRoot);
-
-		// <= 254 chars should be unshortened:
-		Assertions.assertEquals(Strings.repeat("2", 3), namePairs.get("1C6637A8F2E1F75E06FF9984894D6BD16A3A36A9.lng"));
-		Assertions.assertEquals(Strings.repeat("3", 129), namePairs.get("0B51FC45F30A0C3027F2B4C4698C5EFCA3C62FE0.lng"));
-		Assertions.assertEquals(Strings.repeat("4", 130), namePairs.get("CAF8F7708CBF2FD3E735A0C765EBB8E0B879360A.lng"));
-		Assertions.assertEquals(Strings.repeat("5", 250), namePairs.get("1CB1308D10CF786A827C91EEC1FF7B08D91ACAD6.lng"));
-		Assertions.assertEquals(Strings.repeat("6", 251), namePairs.get("C7D6C6201B5344583A9ED2D8F5C3239CCF666230.lng"));
-	}
-
-	@Test
-	public void testMigration() throws IOException {
+	public void testKeyfileGetsUpdates() throws IOException {
 		KeyFile beforeMigration = KeyFile.parse(Files.readAllBytes(masterkeyFile));
 		Assertions.assertEquals(6, beforeMigration.getVersion());
 
