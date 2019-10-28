bd088c4f3f4ea8a2dfe902941b28141a8b81ed62
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2019-10-28 16:04:14 +0100

    restored ability to remove broken dir files from dir listing

--- src/main/java/org/cryptomator/cryptofs/dir/NodeProcessor.java
+++ src/main/java/org/cryptomator/cryptofs/dir/NodeProcessor.java
@@ -9,15 +9,17 @@ import java.util.stream.Stream;
 class NodeProcessor {
 
 	private final C9rProcessor c9rProcessor;
+	private final BrokenDirectoryFilter brokenDirFilter;
 
 	@Inject
-	public NodeProcessor(C9rProcessor c9rProcessor){
+	public NodeProcessor(C9rProcessor c9rProcessor, BrokenDirectoryFilter brokenDirFilter){
 		this.c9rProcessor = c9rProcessor;
+		this.brokenDirFilter = brokenDirFilter;
 	}
 	
 	public Stream<Node> process(Node node) {
 		if (node.fullCiphertextFileName.endsWith(Constants.CRYPTOMATOR_FILE_SUFFIX)) {
-			return c9rProcessor.process(node);
+			return c9rProcessor.process(node).flatMap(brokenDirFilter::process);
 		} else if (node.fullCiphertextFileName.endsWith(Constants.DEFLATED_FILE_SUFFIX)) {
 			return Stream.empty();
 		} else {
