5741bccf94403c0f3f09e6f5dded9d68432b2e7f
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2020-05-25 18:17:36 +0200

    fixes #84

--- src/test/java/org/cryptomator/cryptofs/CryptoFileStoreTest.java
+++ src/test/java/org/cryptomator/cryptofs/CryptoFileStoreTest.java
@@ -9,113 +9,184 @@
 package org.cryptomator.cryptofs;
 
 import org.cryptomator.cryptofs.attr.AttributeViewType;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.file.FileStore;
-import java.nio.file.FileSystem;
-import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.DosFileAttributeView;
+import java.nio.file.attribute.FileAttributeView;
 import java.nio.file.attribute.FileOwnerAttributeView;
 import java.nio.file.attribute.PosixFileAttributeView;
-import java.nio.file.spi.FileSystemProvider;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CryptoFileStoreTest {
-
-	private final Path path = mock(Path.class);
-	private final FileSystemProvider provider = mock(FileSystemProvider.class);
-	private final FileSystem fileSystem = mock(FileSystem.class);
-	private final FileStore delegate = mock(FileStore.class);
+	
 	private final ReadonlyFlag readonlyFlag = mock(ReadonlyFlag.class);
+	
+	@Nested
+	@DisplayName("with delegate present")
+	class DelegatingCryptoFileStoreTest {
 
-	@BeforeEach
-	public void setUp() throws IOException {
-		when(path.getFileSystem()).thenReturn(fileSystem);
-		when(fileSystem.provider()).thenReturn(provider);
-		when(provider.getFileStore(path)).thenReturn(delegate);
+		private final FileStore delegate = mock(FileStore.class);
+		private CryptoFileStore cryptoFileStore;
+
+		@BeforeEach
+		public void setup() {
+			cryptoFileStore = new CryptoFileStore(Optional.of(delegate), readonlyFlag);
+		}
+
+		@Test
+		public void testSupportedFileAttributeViewTypes() {
+			when(delegate.supportsFileAttributeView(BasicFileAttributeView.class)).thenReturn(true);
+			when(delegate.supportsFileAttributeView(PosixFileAttributeView.class)).thenReturn(true);
+			when(delegate.supportsFileAttributeView(DosFileAttributeView.class)).thenReturn(true);
+			when(delegate.supportsFileAttributeView(FileOwnerAttributeView.class)).thenReturn(true);
+			cryptoFileStore = new CryptoFileStore(Optional.of(delegate), readonlyFlag);
+			Set<AttributeViewType> result = cryptoFileStore.supportedFileAttributeViewTypes();
+			MatcherAssert.assertThat(result, CoreMatchers.hasItems(AttributeViewType.BASIC, AttributeViewType.POSIX, AttributeViewType.DOS, AttributeViewType.OWNER));
+		}
+
+		@ParameterizedTest
+		@ValueSource(booleans = {true, false})
+		public void testIsReadonlyWithReadonlyFlag(boolean readonly) {
+			when(readonlyFlag.isSet()).thenReturn(true);
+			when(delegate.isReadOnly()).thenReturn(readonly);
+			Assertions.assertTrue(cryptoFileStore.isReadOnly());
+		}
+
+		@ParameterizedTest
+		@ValueSource(booleans = {true, false})
+		public void testIsReadonlyWithoutReadonlyFlag(boolean readonly) {
+			when(readonlyFlag.isSet()).thenReturn(false);
+			when(delegate.isReadOnly()).thenReturn(readonly);
+			Assertions.assertEquals(readonly, cryptoFileStore.isReadOnly());
+		}
+
+		@ParameterizedTest
+		@ValueSource(longs = {-1, 0, 1, 42, 1337})
+		public void testGetTotalSpace(long num) throws IOException {
+			when(delegate.getTotalSpace()).thenReturn(num);
+			Assertions.assertEquals(num, cryptoFileStore.getTotalSpace());
+		}
+
+		@ParameterizedTest
+		@ValueSource(longs = {-1, 0, 1, 42, 1337})
+		public void testGetUsableSpace(long num) throws IOException {
+			when(delegate.getUsableSpace()).thenReturn(num);
+			Assertions.assertEquals(num, cryptoFileStore.getUsableSpace());
+		}
+
+		@ParameterizedTest
+		@ValueSource(longs = {-1, 0, 1, 42, 1337})
+		public void testGetUnallocatedSpace(long num) throws IOException {
+			when(delegate.getUnallocatedSpace()).thenReturn(num);
+			Assertions.assertEquals(num, cryptoFileStore.getUnallocatedSpace());
+		}
+
+		@ParameterizedTest
+		@ValueSource(classes = {BasicFileAttributeView.class, PosixFileAttributeView.class, DosFileAttributeView.class, FileOwnerAttributeView.class})
+		public void testSupportsFileAttributeViewByType(Class<? extends FileAttributeView> clazz) {
+			when(delegate.supportsFileAttributeView(clazz)).thenReturn(true, false);
+			Assertions.assertTrue(cryptoFileStore.supportsFileAttributeView(clazz));
+			Assertions.assertFalse(cryptoFileStore.supportsFileAttributeView(clazz));
+		}
+
+		@ParameterizedTest
+		@ValueSource(strings = {"basic", "posix", "dos", "owner"})
+		public void testSupportsFileAttributeViewByName(String name) {
+			when(delegate.supportsFileAttributeView(name)).thenReturn(true, false);
+			Assertions.assertTrue(cryptoFileStore.supportsFileAttributeView(name));
+			Assertions.assertFalse(cryptoFileStore.supportsFileAttributeView(name));
+		}
+
+		@Test
+		public void testGetFileStoreAttributeView() {
+			Assertions.assertNull(cryptoFileStore.getFileStoreAttributeView(null));
+		}
+
+		@Test
+		public void testGetAttribute() {
+			Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+				cryptoFileStore.getAttribute(null);
+			});
+		}
+		
 	}
 
-	@Test
-	public void testIsReadonlyReturnsTrueIfReadonlyFlagIsSet() {
-		when(readonlyFlag.isSet()).thenReturn(true);
+	@Nested
+	@DisplayName("with delegate absent")
+	class FallbackCryptoFileStoreTest {
+		
+		private CryptoFileStore cryptoFileStore;
+		
+		@BeforeEach
+		public void setup() {
+			cryptoFileStore = new CryptoFileStore(Optional.empty(), readonlyFlag);
+		}
+		
+		@Test
+		public void testSupportedFileAttributeViewTypes() {
+			Set<AttributeViewType> result = cryptoFileStore.supportedFileAttributeViewTypes();
+			MatcherAssert.assertThat(result, CoreMatchers.hasItems(AttributeViewType.BASIC));
+		}
 
-		CryptoFileStore inTest = new CryptoFileStore(path, readonlyFlag);
+		@Test
+		public void testIsReadonly() {
+			when(readonlyFlag.isSet()).thenReturn(true, false);
+			Assertions.assertTrue(cryptoFileStore.isReadOnly());
+			Assertions.assertFalse(cryptoFileStore.isReadOnly());
+		}
+		
+		@Test
+		public void testGetTotalSpace() throws IOException {
+			Assertions.assertEquals(CryptoFileStore.DEFAULT_TOTAL_SPACE, cryptoFileStore.getTotalSpace());
+		}
 
-		Assertions.assertTrue(inTest.isReadOnly());
-	}
+		@Test
+		public void testGetUsableSpace() throws IOException {
+			Assertions.assertEquals(CryptoFileStore.DEFAULT_USABLE_SPACE, cryptoFileStore.getUsableSpace());
+		}
 
-	@Test
-	public void testIsReadonlyReturnsFalseIfReadonlyFlagIsNotSet() {
-		when(readonlyFlag.isSet()).thenReturn(false);
+		@Test
+		public void testGetUnallocatedSpace() throws IOException {
+			Assertions.assertEquals(CryptoFileStore.DEFAULT_UNALLOCATED_SPACE, cryptoFileStore.getUnallocatedSpace());
+		}
 
-		CryptoFileStore inTest = new CryptoFileStore(path, readonlyFlag);
+		@Test
+		public void testSupportsFileAttributeViewByType() {
+			Assertions.assertTrue(cryptoFileStore.supportsFileAttributeView(BasicFileAttributeView.class));
+		}
 
-		Assertions.assertFalse(inTest.isReadOnly());
-	}
+		@Test
+		public void testSupportsFileAttributeViewByName() {
+			Assertions.assertTrue(cryptoFileStore.supportsFileAttributeView("basic"));
+		}
+		
+		@Test
+		public void testGetFileStoreAttributeView() {
+			Assertions.assertNull(cryptoFileStore.getFileStoreAttributeView(null));
+		}
 
-	@Test
-	public void testSupportedFileAttributeViewTypes() {
-		when(delegate.supportsFileAttributeView(PosixFileAttributeView.class)).thenReturn(true);
-		when(delegate.supportsFileAttributeView(FileOwnerAttributeView.class)).thenReturn(true);
-		when(delegate.supportsFileAttributeView(BasicFileAttributeView.class)).thenReturn(true);
-		when(delegate.supportsFileAttributeView(DosFileAttributeView.class)).thenReturn(false);
+		@Test
+		public void testGetAttribute() {
+			Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+				cryptoFileStore.getAttribute(null);
+			});
+		}
 
-		CryptoFileStore inTest = new CryptoFileStore(path, readonlyFlag);
-
-		Set<AttributeViewType> result = inTest.supportedFileAttributeViewTypes();
-		Assertions.assertTrue(result.contains(AttributeViewType.POSIX));
-		Assertions.assertTrue(result.contains(AttributeViewType.OWNER));
-		Assertions.assertTrue(result.contains(AttributeViewType.BASIC));
-		Assertions.assertFalse(result.contains(AttributeViewType.DOS));
-	}
-
-	@Test
-	public void testSupportsFileAttributeViewByType() {
-		when(delegate.supportsFileAttributeView(PosixFileAttributeView.class)).thenReturn(true);
-		when(delegate.supportsFileAttributeView(FileOwnerAttributeView.class)).thenReturn(true);
-		when(delegate.supportsFileAttributeView(BasicFileAttributeView.class)).thenReturn(true);
-		when(delegate.supportsFileAttributeView(DosFileAttributeView.class)).thenReturn(false);
-
-		CryptoFileStore inTest = new CryptoFileStore(path, readonlyFlag);
-
-		Assertions.assertTrue(inTest.supportsFileAttributeView(PosixFileAttributeView.class));
-		Assertions.assertTrue(inTest.supportsFileAttributeView(FileOwnerAttributeView.class));
-		Assertions.assertTrue(inTest.supportsFileAttributeView(BasicFileAttributeView.class));
-		Assertions.assertFalse(inTest.supportsFileAttributeView(DosFileAttributeView.class));
-	}
-
-	@Test
-	public void testSupportsFileAttributeViewByName() {
-		when(delegate.supportsFileAttributeView(PosixFileAttributeView.class)).thenReturn(true);
-		when(delegate.supportsFileAttributeView(FileOwnerAttributeView.class)).thenReturn(true);
-		when(delegate.supportsFileAttributeView(BasicFileAttributeView.class)).thenReturn(true);
-		when(delegate.supportsFileAttributeView(DosFileAttributeView.class)).thenReturn(false);
-
-		CryptoFileStore inTest = new CryptoFileStore(path, readonlyFlag);
-
-		Assertions.assertTrue(inTest.supportsFileAttributeView("posix"));
-		Assertions.assertTrue(inTest.supportsFileAttributeView("owner"));
-		Assertions.assertTrue(inTest.supportsFileAttributeView("basic"));
-		Assertions.assertFalse(inTest.supportsFileAttributeView("dos"));
-	}
-
-	@Test
-	public void testRethrowsIOExceptionFromGetFileStoreUnchecked() throws IOException {
-		IOException expected = new IOException();
-		when(provider.getFileStore(path)).thenThrow(expected);
-
-		UncheckedIOException e = Assertions.assertThrows(UncheckedIOException.class, () -> {
-			new CryptoFileStore(path, readonlyFlag);
-		});
-		Assertions.assertSame(expected, e.getCause());
 	}
 
 }
