e719b8e5043ff1c972b3481ff1677db583289844
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2020-11-20 15:12:41 +0100

    Added vault version 8 migrator (references #95)

---
+++ src/main/java/org/cryptomator/cryptofs/migration/v8/Version8Migrator.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Skymatic UG (haftungsbeschränkt).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the accompanying LICENSE file.
+ *******************************************************************************/
+package org.cryptomator.cryptofs.migration.v8;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.cryptomator.cryptofs.common.MasterkeyBackupHelper;
+import org.cryptomator.cryptofs.migration.api.MigrationContinuationListener;
+import org.cryptomator.cryptofs.migration.api.MigrationProgressListener;
+import org.cryptomator.cryptofs.migration.api.Migrator;
+import org.cryptomator.cryptolib.api.Cryptor;
+import org.cryptomator.cryptolib.api.CryptorProvider;
+import org.cryptomator.cryptolib.api.InvalidPassphraseException;
+import org.cryptomator.cryptolib.api.KeyFile;
+import org.cryptomator.cryptolib.api.UnsupportedVaultFormatException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+import java.util.Arrays;
+import java.util.UUID;
+
+public class Version8Migrator implements Migrator {
+
+	private static final String CONFIG_FILE_NAME = "vaultconfig.cryptomator";
+	private static final Logger LOG = LoggerFactory.getLogger(Version8Migrator.class);
+
+	private final CryptorProvider cryptorProvider;
+
+	@Inject
+	public Version8Migrator(CryptorProvider cryptorProvider) {
+		this.cryptorProvider = cryptorProvider;
+	}
+
+	@Override
+	public void migrate(Path vaultRoot, String masterkeyFilename, CharSequence passphrase, MigrationProgressListener progressListener, MigrationContinuationListener continuationListener) throws InvalidPassphraseException, UnsupportedVaultFormatException, IOException {
+		LOG.info("Upgrading {} from version 7 to version 8.", vaultRoot);
+		progressListener.update(MigrationProgressListener.ProgressState.INITIALIZING, 0.0);
+		Path masterkeyFile = vaultRoot.resolve(masterkeyFilename);
+		Path vaultConfigFile = vaultRoot.resolve(CONFIG_FILE_NAME);
+		byte[] fileContentsBeforeUpgrade = Files.readAllBytes(masterkeyFile);
+		byte[] rawKey = new byte[0];
+		KeyFile keyFile = KeyFile.parse(fileContentsBeforeUpgrade);
+		try (Cryptor cryptor = cryptorProvider.createFromKeyFile(keyFile, passphrase, 7)) {
+			// create backup, as soon as we know the password was correct:
+			Path masterkeyBackupFile = MasterkeyBackupHelper.attemptMasterKeyBackup(masterkeyFile);
+			LOG.info("Backed up masterkey from {} to {}.", masterkeyFile.getFileName(), masterkeyBackupFile.getFileName());
+
+			// create vaultconfig.cryptomator
+			rawKey = cryptor.getRawKey();
+			Algorithm algorithm = Algorithm.HMAC256(rawKey);
+			var config = JWT.create() //
+					.withJWTId(UUID.randomUUID().toString()) //
+					.withClaim("format", 8) //
+					.withClaim("keysource", "MASTERKEY_FILE") //
+					.withClaim("ciphermode", "SIV_CTRMAC") //
+					.withClaim("maxFileNameLen", 220) //
+					.sign(algorithm);
+			Files.writeString(vaultConfigFile, config, StandardCharsets.US_ASCII, StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW);
+			LOG.info("Wrote vault config to {}.", vaultConfigFile);
+
+			progressListener.update(MigrationProgressListener.ProgressState.FINALIZING, 0.0);
+
+			// rewrite masterkey file with normalized passphrase:
+			byte[] fileContentsAfterUpgrade = cryptor.writeKeysToMasterkeyFile(passphrase, 999).serialize();
+			Files.write(masterkeyFile, fileContentsAfterUpgrade, StandardOpenOption.TRUNCATE_EXISTING);
+			LOG.info("Updated masterkey.");
+		} finally {
+			Arrays.fill(rawKey, (byte) 0x00);
+		}
+		LOG.info("Upgraded {} from version 7 to version 8.", vaultRoot);
+	}
+
+}
---
+++ src/test/java/org/cryptomator/cryptofs/migration/v8/Version8MigratorTest.java
@@ -0,0 +1,67 @@
+package org.cryptomator.cryptofs.migration.v8;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import org.cryptomator.cryptofs.migration.api.Migrator;
+import org.cryptomator.cryptofs.mocks.NullSecureRandom;
+import org.cryptomator.cryptolib.Cryptors;
+import org.cryptomator.cryptolib.api.CryptorProvider;
+import org.cryptomator.cryptolib.api.KeyFile;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Version8MigratorTest {
+
+	private FileSystem fs;
+	private Path pathToVault;
+	private Path masterkeyFile;
+	private Path vaultConfigFile;
+	private CryptorProvider cryptorProvider;
+
+	@BeforeEach
+	public void setup() throws IOException {
+		cryptorProvider = Cryptors.version1(NullSecureRandom.INSTANCE);
+		fs = Jimfs.newFileSystem(Configuration.unix());
+		pathToVault = fs.getPath("/vaultDir");
+		masterkeyFile = pathToVault.resolve("masterkey.cryptomator");
+		vaultConfigFile = pathToVault.resolve("vaultconfig.cryptomator");
+		Files.createDirectory(pathToVault);
+	}
+
+	@AfterEach
+	public void teardown() throws IOException {
+		fs.close();
+	}
+
+	@Test
+	public void testMigrate() throws IOException {
+		KeyFile beforeMigration = cryptorProvider.createNew().writeKeysToMasterkeyFile("topsecret", 7);
+		Assumptions.assumeTrue(beforeMigration.getVersion() == 7);
+		Assumptions.assumeFalse(Files.exists(vaultConfigFile));
+		Files.write(masterkeyFile, beforeMigration.serialize());
+
+		Migrator migrator = new Version8Migrator(cryptorProvider);
+		migrator.migrate(pathToVault, masterkeyFile.getFileName().toString(), "topsecret");
+
+		KeyFile afterMigration = KeyFile.parse(Files.readAllBytes(masterkeyFile));
+		Assertions.assertEquals(999, afterMigration.getVersion());
+		Assertions.assertTrue(Files.exists(vaultConfigFile));
+		DecodedJWT token = JWT.decode(Files.readString(vaultConfigFile));
+		Assertions.assertNotNull(token.getId());
+		Assertions.assertEquals(8, token.getClaim("format").asInt());
+		Assertions.assertEquals("SIV_CTRMAC", token.getClaim("ciphermode").asString());
+		Assertions.assertEquals("MASTERKEY_FILE", token.getClaim("keysource").asString());
+		Assertions.assertEquals(220, token.getClaim("maxFileNameLen").asInt());
+	}
+
+}
