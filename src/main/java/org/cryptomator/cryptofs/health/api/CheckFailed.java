dc6f5ac16276061536b8e0e3651068a65c3cac2b
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2021-01-20 14:15:55 +0100

    fixed class

--- src/main/java/org/cryptomator/cryptofs/health/api/CheckFailed.java
+++ src/main/java/org/cryptomator/cryptofs/health/api/CheckFailed.java
@@ -2,7 +2,15 @@ package org.cryptomator.cryptofs.health.api;
 
 public class CheckFailed extends DiagnosticResult {
 
+	private final String message;
+
 	public CheckFailed(String message) {
-		super(Severity.CRITICAL, message);
+		super(Severity.CRITICAL);
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Check failed: %s", message);
 	}
 }
