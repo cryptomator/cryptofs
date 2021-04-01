5ebbd294fed0191a112956e0f7c3cbb754c13fe7
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2021-04-01 16:20:59 +0200

    Use ServiceLoader to access different health checks

---
+++ src/test/java/org/cryptomator/cryptofs/health/api/HealthChecksTest.java
@@ -0,0 +1,50 @@
+package org.cryptomator.cryptofs.health.api;
+
+import org.cryptomator.cryptofs.VaultConfig;
+import org.cryptomator.cryptolib.common.MasterkeyFileAccess;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+class HealthChecksTest {
+
+	@Test
+	public void testGetAll() {
+		var result = HealthCheck.allChecks();
+
+		Assertions.assertFalse(result.isEmpty());
+	}
+
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Enter path to vault:");
+			Path p = Paths.get(scanner.nextLine());
+
+			var masterkeyFileAccess = new MasterkeyFileAccess(new byte[0], SecureRandom.getInstanceStrong());
+			var masterkeyFile = p.resolve("masterkey.cryptomator");
+			var vaultConfigFile = p.resolve("vault.cryptomator");
+			var unverifiedCfg = VaultConfig.decode(Files.readString(vaultConfigFile));
+
+			System.out.println("Enter password [WARNING: not obfuscated]");
+			String pw = scanner.nextLine();
+
+			try (var masterkey = masterkeyFileAccess.load(masterkeyFile, pw)) {
+				var verifiedCfg = unverifiedCfg.verify(masterkey.getEncoded(), unverifiedCfg.allegedVaultVersion());
+				var cryptor = verifiedCfg.getCipherCombo().getCryptorProvider(SecureRandom.getInstanceStrong()).withKey(masterkey);
+				HealthCheck.allChecks().forEach(check -> {
+					System.out.println("Running " + check.identifier());
+					var results = check.check(p, verifiedCfg, masterkey, cryptor);
+					results.forEach(System.out::println);
+				});
+			}
+		}
+	}
+
+}
