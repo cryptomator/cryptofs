38b8e943a6f3ab68b923b87e650753bd04139d40
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2021-04-09 12:37:50 +0200

    fix deadlock (hopefully)

--- src/main/java/org/cryptomator/cryptofs/health/api/AbstractHealthCheck.java
+++ src/main/java/org/cryptomator/cryptofs/health/api/AbstractHealthCheck.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
-import java.util.Iterator;
 import java.util.Spliterators;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutorService;
@@ -19,10 +18,6 @@ import java.util.function.Consumer;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import static java.util.Spliterator.DISTINCT;
-import static java.util.Spliterator.IMMUTABLE;
-import static java.util.Spliterator.NONNULL;
-
 public abstract class AbstractHealthCheck implements HealthCheck {
 
 	private static final Logger LOG = LoggerFactory.getLogger(AbstractHealthCheck.class);
@@ -32,29 +27,29 @@ public abstract class AbstractHealthCheck implements HealthCheck {
 
 	@Override
 	public final Stream<DiagnosticResult> check(Path pathToVault, VaultConfig config, Masterkey masterkey, Cryptor cryptor, ExecutorService executor) {
-		ResultIterator resultIterator = new ResultIterator();
+		ResultSpliterator resultSpliterator = new ResultSpliterator();
 
 		task = executor.submit(() -> {
 			try {
-				check(pathToVault, config, masterkey, cryptor, resultIterator);
+				check(pathToVault, config, masterkey, cryptor, resultSpliterator);
 			} catch (CancellationException e) {
 				assert cancelled.get();
-				LOG.info("{} cancelled.", identifier());
+				LOG.debug("{} cancelled.", identifier());
 			} finally {
-				resultIterator.end();
+				resultSpliterator.end();
 			}
 		});
 
-		return StreamSupport.stream(Spliterators.spliteratorUnknownSize(resultIterator, DISTINCT | NONNULL | IMMUTABLE), false);
+		return StreamSupport.stream(resultSpliterator, false);
 	}
 
 	/**
 	 * Checks the vault at the given path.
 	 *
-	 * @param pathToVault Path to the vault's root directory
-	 * @param config The parsed and verified vault config
-	 * @param masterkey The masterkey
-	 * @param cryptor A cryptor initialized for this vault
+	 * @param pathToVault     Path to the vault's root directory
+	 * @param config          The parsed and verified vault config
+	 * @param masterkey       The masterkey
+	 * @param cryptor         A cryptor initialized for this vault
 	 * @param resultCollector A callback collecting results. Invoking this method may block until the result is processed.
 	 */
 	protected abstract void check(Path pathToVault, VaultConfig config, Masterkey masterkey, Cryptor cryptor, Consumer<DiagnosticResult> resultCollector);
@@ -67,29 +62,30 @@ public abstract class AbstractHealthCheck implements HealthCheck {
 		}
 	}
 
-	private class ResultIterator implements Iterator<DiagnosticResult>, Consumer<DiagnosticResult> {
+	private class ResultSpliterator extends Spliterators.AbstractSpliterator<DiagnosticResult> implements Consumer<DiagnosticResult> {
 
 		private static final DiagnosticResult POISON = new PoisonResult();
-		private final TransferQueue<DiagnosticResult> queue = new LinkedTransferQueue<>();
-		private DiagnosticResult currentElement;
 
-		@Override
-		public boolean hasNext() {
-			if (cancelled.get()) {
-				return false;
-			}
-			try {
-				this.currentElement = queue.take();
-				return currentElement != POISON;
-			} catch (InterruptedException e) {
-				Thread.currentThread().interrupt();
-				return false; // prevent further iteration
-			}
+		private final TransferQueue<DiagnosticResult> queue = new LinkedTransferQueue<>();
+
+		public ResultSpliterator() {
+			super(Long.MAX_VALUE, DISTINCT | NONNULL | IMMUTABLE);
 		}
 
 		@Override
-		public DiagnosticResult next() {
-			return currentElement;
+		public boolean tryAdvance(Consumer<? super DiagnosticResult> action) {
+			try {
+				var result = queue.take();
+				if (result == POISON) {
+					return false;
+				} else {
+					action.accept(result);
+					return true;
+				}
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+				return false;
+			}
 		}
 
 		@Override
@@ -101,9 +97,7 @@ public abstract class AbstractHealthCheck implements HealthCheck {
 				queue.transfer(diagnosticResult);
 			} catch (InterruptedException e) {
 				Thread.currentThread().interrupt();
-				if (cancelled.get()) {
-					throw new CancellationException();
-				}
+				throw new CancellationException();
 			}
 		}
 
--- src/test/java/org/cryptomator/cryptofs/health/api/AbstractHealthCheckTest.java
+++ src/test/java/org/cryptomator/cryptofs/health/api/AbstractHealthCheckTest.java
@@ -5,6 +5,7 @@ import org.cryptomator.cryptolib.api.Cryptor;
 import org.cryptomator.cryptolib.api.Masterkey;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -16,13 +17,14 @@ import java.util.function.Consumer;
 
 class AbstractHealthCheckTest {
 
-	@Test
+	private Path pathToVault = Mockito.mock(Path.class);
+	private VaultConfig config = Mockito.mock(VaultConfig.class);
+	private Masterkey masterkey = Mockito.mock(Masterkey.class);
+	private Cryptor cryptor = Mockito.mock(Cryptor.class);
+
+	@RepeatedTest(100)
 	@DisplayName("stream terminates after reading all results")
 	public void testConsumeStream() {
-		Path pathToVault = Mockito.mock(Path.class);
-		VaultConfig config = Mockito.mock(VaultConfig.class);
-		Masterkey masterkey = Mockito.mock(Masterkey.class);
-		Cryptor cryptor = Mockito.mock(Cryptor.class);
 		DiagnosticResult result1 = Mockito.mock(DiagnosticResult.class, "result1");
 		DiagnosticResult result2 = Mockito.mock(DiagnosticResult.class, "result2");
 		AbstractHealthCheck check = new AbstractHealthCheck() {
@@ -36,19 +38,13 @@ class AbstractHealthCheckTest {
 		var stream = check.check(pathToVault, config, masterkey, cryptor);
 
 		Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
-			CountDownLatch cdl = new CountDownLatch(2);
-			stream.forEach(result -> cdl.countDown());
-			cdl.await();
+			Assertions.assertEquals(2, stream.count());
 		});
 	}
 
 	@Test
 	@DisplayName("health check waits if result is not consumed")
 	public void testCheckerBlocks() throws InterruptedException {
-		Path pathToVault = Mockito.mock(Path.class);
-		VaultConfig config = Mockito.mock(VaultConfig.class);
-		Masterkey masterkey = Mockito.mock(Masterkey.class);
-		Cryptor cryptor = Mockito.mock(Cryptor.class);
 		DiagnosticResult result1 = Mockito.mock(DiagnosticResult.class, "result1");
 		CountDownLatch cdl1 = new CountDownLatch(1);
 		CountDownLatch cdl2 = new CountDownLatch(1);
@@ -67,33 +63,36 @@ class AbstractHealthCheckTest {
 		Assertions.assertFalse(cdl2.await(1, TimeUnit.SECONDS), "must not reach cdl2");
 	}
 
-	@Test
+	@RepeatedTest(100)
 	@DisplayName("health check can be cancelled")
 	public void testCancel() {
-		Path pathToVault = Mockito.mock(Path.class);
-		VaultConfig config = Mockito.mock(VaultConfig.class);
-		Masterkey masterkey = Mockito.mock(Masterkey.class);
-		Cryptor cryptor = Mockito.mock(Cryptor.class);
 		DiagnosticResult result1 = Mockito.mock(DiagnosticResult.class, "result1");
 		DiagnosticResult result2 = Mockito.mock(DiagnosticResult.class, "result2");
+		CountDownLatch cdl1 = new CountDownLatch(1);
 		AbstractHealthCheck check = new AbstractHealthCheck() {
 			@Override
 			protected void check(Path pathToVault, VaultConfig config, Masterkey masterkey, Cryptor cryptor, Consumer<DiagnosticResult> resultCollector) {
 				resultCollector.accept(result1);
+				try {
+					cdl1.await();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 				resultCollector.accept(result2);
-				System.out.println("ad");
 			}
 		};
 
 		var stream = check.check(pathToVault, config, masterkey, cryptor);
 
 		Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
-			stream.forEach(result -> {
+			var results = stream.peek(result -> {
 				if (result == result1) {
+					// cancel check after first result
 					check.cancel();
+					cdl1.countDown();
 				}
-				Assertions.assertNotEquals(result2, result);
-			});
+			}).toArray(DiagnosticResult[]::new);
+			Assertions.assertArrayEquals(new DiagnosticResult[]{result1}, results);
 		});
 	}
 
