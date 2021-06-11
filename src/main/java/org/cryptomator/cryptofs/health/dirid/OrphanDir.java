836fcd9097a688ddd4ece5faafd7ddd9d445e2a2
Armin Schrenk <armin.schrenk@skymatic.de>
2021-06-11 09:27:11 +0200

    the plan

--- src/main/java/org/cryptomator/cryptofs/health/dirid/OrphanDir.java
+++ src/main/java/org/cryptomator/cryptofs/health/dirid/OrphanDir.java
@@ -1,7 +1,13 @@
 package org.cryptomator.cryptofs.health.dirid;
 
+import org.cryptomator.cryptofs.CryptoPath;
+import org.cryptomator.cryptofs.CryptoPathMapper;
+import org.cryptomator.cryptofs.VaultConfig;
 import org.cryptomator.cryptofs.health.api.DiagnosticResult;
+import org.cryptomator.cryptolib.api.Cryptor;
+import org.cryptomator.cryptolib.api.Masterkey;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
@@ -34,4 +40,21 @@ public class OrphanDir implements DiagnosticResult {
 	}
 
 	// fix: create new dirId inside of L+F dir and rename existing dir accordingly.
+	@Override
+	public void fix(Path pathToVault, VaultConfig config, Masterkey masterkey, Cryptor cryptor) {
+		// open filesystem
+			//check if l+f dir exists
+			// if not, create it
+			// create new directory inside l+f with cleartext name based one orphandir path
+		// close filesystem
+
+		//find out dirId and ciphertextPath to the created directory
+
+		//for each resource in orphaned ciphertext dir:
+		//	move resource to unfiddled dir in l+f and rename resource  (use dirId for associated data)
+		//  distinct by directory, symlink and file
+		//  e.g. file1, file2, file3,...dir1, dir2, dir3,...,etc
+		//  don't forget shortend resource names
+		//remove empty, orphaned ciphertextdir
+	}
 }
