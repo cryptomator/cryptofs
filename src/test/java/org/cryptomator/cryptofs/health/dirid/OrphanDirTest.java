3073111ee6e156c52e5e93afb6341b8309da35de
Armin Schrenk <armin.schrenk@skymatic.de>
2021-06-17 10:40:49 +0200

    Add unit test

---
+++ src/test/java/org/cryptomator/cryptofs/health/dirid/OrphanDirTest.java
@@ -0,0 +1,89 @@
+package org.cryptomator.cryptofs.health.dirid;
+
+import com.google.common.io.BaseEncoding;
+import jdk.jfr.Name;
+import org.cryptomator.cryptofs.common.Constants;
+import org.cryptomator.cryptolib.api.FileNameCryptor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.Mockito;
+
+import javax.inject.Named;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.UUID;
+
+public class OrphanDirTest {
+
+	@TempDir
+	public Path pathToVault;
+
+	@Test
+	public void testPrepareCryptoFileSystemNonExistingRecoveryDir() throws IOException {
+		Path p = Mockito.mock(Path.class, "ignored");
+		OrphanDir result = new OrphanDir(p);
+
+
+		Path cipherRootDir = pathToVault.resolve("d/00/0000");
+		Files.createDirectories(cipherRootDir);
+		String clearStepParentName = "step-parent";
+		FileNameCryptor cryptor = Mockito.mock(FileNameCryptor.class);
+		Mockito.doReturn("000000").when(cryptor).hashDirectoryId(Constants.ROOT_DIR_ID);
+		Mockito.doReturn("111111").when(cryptor).hashDirectoryId(Constants.RECOVERY_DIR_ID);
+		Mockito.doReturn("222222").when(cryptor).hashDirectoryId("aaaaaa");
+
+		Mockito.doReturn("1").when(cryptor).encryptFilename(BaseEncoding.base64Url(), Constants.RECOVERY_DIR_NAME, Constants.ROOT_DIR_ID.getBytes(StandardCharsets.UTF_8));
+		Mockito.doReturn("2").when(cryptor).encryptFilename(BaseEncoding.base64Url(), clearStepParentName, Constants.RECOVERY_DIR_ID.getBytes(StandardCharsets.UTF_8));
+
+		try (var uuidClass = Mockito.mockStatic(UUID.class)) {
+			UUID uuid = Mockito.mock(UUID.class);
+			uuidClass.when(() -> UUID.randomUUID()).thenReturn(uuid);
+			Mockito.doReturn("aaaaaa").when(uuid).toString();
+
+			result.prepareCryptoFilesystem(pathToVault, cryptor, clearStepParentName);
+		}
+
+		Assertions.assertEquals(Constants.RECOVERY_DIR_ID, Files.readString(pathToVault.resolve("d/00/0000/1.c9r/dir.c9r"), StandardCharsets.UTF_8));
+		Assertions.assertEquals("aaaaaa", Files.readString(pathToVault.resolve("d/11/1111/2.c9r/dir.c9r"), StandardCharsets.UTF_8));
+		Assertions.assertTrue(Files.isDirectory(pathToVault.resolve("d/22/2222")));
+
+	}
+
+	@Test
+	@Name("test prepare fs method with a user created recovery id")
+	public void testPrepareCryptoFSWithRecoveryDirWrongId() throws IOException {
+		Path p = Mockito.mock(Path.class, "ignored");
+		OrphanDir result = new OrphanDir(p);
+
+		Path userCreatedRecoveryDirFile = pathToVault.resolve("d/00/0000/1.c9r/dir.c9r");
+		Files.createDirectories(userCreatedRecoveryDirFile.getParent());
+		Files.writeString(userCreatedRecoveryDirFile,UUID.randomUUID().toString(),StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+
+		String clearStepParentName = "step-parent";
+		FileNameCryptor cryptor = Mockito.mock(FileNameCryptor.class);
+		Mockito.doReturn("000000").when(cryptor).hashDirectoryId(Constants.ROOT_DIR_ID);
+		Mockito.doReturn("111111").when(cryptor).hashDirectoryId(Constants.RECOVERY_DIR_ID);
+		Mockito.doReturn("222222").when(cryptor).hashDirectoryId("aaaaaa");
+
+		Mockito.doReturn("1").when(cryptor).encryptFilename(BaseEncoding.base64Url(), Constants.RECOVERY_DIR_NAME, Constants.ROOT_DIR_ID.getBytes(StandardCharsets.UTF_8));
+		Mockito.doReturn("2").when(cryptor).encryptFilename(BaseEncoding.base64Url(), clearStepParentName, Constants.RECOVERY_DIR_ID.getBytes(StandardCharsets.UTF_8));
+
+		try (var uuidClass = Mockito.mockStatic(UUID.class)) {
+			UUID uuid = Mockito.mock(UUID.class);
+			uuidClass.when(() -> UUID.randomUUID()).thenReturn(uuid);
+			Mockito.doReturn("aaaaaa").when(uuid).toString();
+
+			Assertions.assertThrows(FileAlreadyExistsException.class, () -> result.prepareCryptoFilesystem(pathToVault, cryptor, clearStepParentName));
+		}
+
+		Assertions.assertNotEquals(Constants.RECOVERY_DIR_ID, Files.readString(pathToVault.resolve("d/00/0000/1.c9r/dir.c9r"), StandardCharsets.UTF_8));
+		Assertions.assertTrue(Files.notExists(pathToVault.resolve("d/11/1111/2.c9r/dir.c9r")));
+		Assertions.assertTrue(Files.notExists(pathToVault.resolve("d/22/2222")));
+
+	}
+}
