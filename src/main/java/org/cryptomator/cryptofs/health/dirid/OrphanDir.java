a304bd06eb403990c8535d3981ae04e0173ed8ba
Armin Schrenk <armin.schrenk@zoho.eu>
2021-06-24 16:08:07 +0200

    Health-API: Implement OrphanDir.fix() (#106)
    * introduce new directory with fixed dirId CRYPTOMATOR_RECOVERY
    * a stepparent dir is always created inside this dir
    * adopted files are suffixd with a random id constant over each fix() attempt
    * fix mockito to reflectively access certain classes (fixes #107)
    * remove ATOMIC_MOVE and manual code formatting
    
    Co-authored-by: Sebastian Stenzel <sebastian.stenzel@gmail.com>

--- src/main/java/org/cryptomator/cryptofs/health/dirid/OrphanDir.java
+++ src/main/java/org/cryptomator/cryptofs/health/dirid/OrphanDir.java
@@ -1,9 +1,28 @@
 package org.cryptomator.cryptofs.health.dirid;
 
+import com.google.common.io.BaseEncoding;
+import org.cryptomator.cryptofs.CryptoPathMapper;
+import org.cryptomator.cryptofs.VaultConfig;
+import org.cryptomator.cryptofs.common.CiphertextFileType;
+import org.cryptomator.cryptofs.common.Constants;
 import org.cryptomator.cryptofs.health.api.DiagnosticResult;
+import org.cryptomator.cryptolib.api.Cryptor;
+import org.cryptomator.cryptolib.api.FileNameCryptor;
+import org.cryptomator.cryptolib.api.Masterkey;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.cryptomator.cryptofs.health.api.CommonDetailKeys.ENCRYPTED_PATH;
 
@@ -11,6 +30,11 @@ import static org.cryptomator.cryptofs.health.api.CommonDetailKeys.ENCRYPTED_PAT
  * An orphan directory is a detached node, not referenced by any dir.c9r file.
  */
 public class OrphanDir implements DiagnosticResult {
+
+	private static final String FILE_PREFIX = "file";
+	private static final String DIR_PREFIX = "directory";
+	private static final String SYMLINK_PREFIX = "symlink";
+	private static final String LONG_NAME_SUFFIX_BASE = "_withVeryLongName";
 
 	final Path dir;
 
@@ -33,5 +57,127 @@ public class OrphanDir implements DiagnosticResult {
 		return Map.of(ENCRYPTED_PATH, dir.toString());
 	}
 
-	// fix: create new dirId inside of L+F dir and rename existing dir accordingly.
+	@Override
+	public void fix(Path pathToVault, VaultConfig config, Masterkey masterkey, Cryptor cryptor) throws IOException {
+		var sha1 = getSha1MessageDigest();
+		String runId = Integer.toString((short) UUID.randomUUID().getMostSignificantBits(), 32);
+		Path dataDir = pathToVault.resolve(Constants.DATA_DIR_NAME);
+		Path orphanedDir = dataDir.resolve(this.dir);
+		String orphanDirIdHash = dir.getParent().getFileName().toString() + dir.getFileName().toString();
+
+		Path recoveryDir = prepareRecoveryDir(pathToVault, cryptor.fileNameCryptor());
+		if (recoveryDir.toAbsolutePath().equals(orphanedDir.toAbsolutePath())) {
+			return; //recovery dir was orphaned, already recovered by prepare method
+		}
+
+		var stepParentDir = prepareStepParent(dataDir, recoveryDir, cryptor.fileNameCryptor(), orphanDirIdHash);
+		AtomicInteger fileCounter = new AtomicInteger(1);
+		AtomicInteger dirCounter = new AtomicInteger(1);
+		AtomicInteger symlinkCounter = new AtomicInteger(1);
+		String longNameSuffix = createClearnameToBeShortened(config.getShorteningThreshold());
+		try (var orphanedContentStream = Files.newDirectoryStream(orphanedDir)) {
+			for (Path orphanedResource : orphanedContentStream) {
+				//@formatter:off
+				var newClearName = switch (determineCiphertextFileType(orphanedResource)) {
+						case FILE -> FILE_PREFIX + fileCounter.getAndIncrement();
+						case DIRECTORY -> DIR_PREFIX + dirCounter.getAndIncrement();
+						case SYMLINK -> SYMLINK_PREFIX + symlinkCounter.getAndIncrement();
+					} + "_" + runId;
+				//@formatter:on
+				adoptOrphanedResource(orphanedResource, newClearName, stepParentDir, cryptor.fileNameCryptor(), longNameSuffix, sha1);
+			}
+		}
+		Files.delete(orphanedDir);
+	}
+
+	//visible for testing
+	Path prepareRecoveryDir(Path pathToVault, FileNameCryptor cryptor) throws IOException {
+		Path dataDir = pathToVault.resolve(Constants.DATA_DIR_NAME);
+		String rootDirHash = cryptor.hashDirectoryId(Constants.ROOT_DIR_ID);
+		Path vaultCipherRootPath = dataDir.resolve(rootDirHash.substring(0, 2)).resolve(rootDirHash.substring(2)).toAbsolutePath();
+
+		//check if recovery dir exists and has unique recovery id
+		String cipherRecoveryDirName = convertClearToCiphertext(cryptor, Constants.RECOVERY_DIR_NAME, Constants.ROOT_DIR_ID);
+		Path cipherRecoveryDirFile = vaultCipherRootPath.resolve(cipherRecoveryDirName + "/" + Constants.DIR_FILE_NAME);
+		if (Files.notExists(cipherRecoveryDirFile, LinkOption.NOFOLLOW_LINKS)) {
+			Files.createDirectories(cipherRecoveryDirFile.getParent());
+			Files.writeString(cipherRecoveryDirFile, Constants.RECOVERY_DIR_ID, StandardCharsets.UTF_8, StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE);
+		} else {
+			String uuid = Files.readString(cipherRecoveryDirFile, StandardCharsets.UTF_8);
+			if (!Constants.RECOVERY_DIR_ID.equals(uuid)) {
+				throw new FileAlreadyExistsException("Directory /" + Constants.RECOVERY_DIR_NAME + " already exists, but with wrong directory id.");
+			}
+		}
+		String recoveryDirHash = cryptor.hashDirectoryId(Constants.RECOVERY_DIR_ID);
+		Path cipherRecoveryDir = dataDir.resolve(recoveryDirHash.substring(0, 2)).resolve(recoveryDirHash.substring(2)).toAbsolutePath();
+		Files.createDirectories(cipherRecoveryDir);
+
+		return cipherRecoveryDir;
+	}
+
+	// visible for testing
+	CryptoPathMapper.CiphertextDirectory prepareStepParent(Path dataDir, Path cipherRecoveryDir, FileNameCryptor cryptor, String clearStepParentDirName) throws IOException {
+		//create "step-parent" directory to move orphaned files to
+		String cipherStepParentDirName = convertClearToCiphertext(cryptor, clearStepParentDirName, Constants.RECOVERY_DIR_ID);
+		Path cipherStepParentDirFile = cipherRecoveryDir.resolve(cipherStepParentDirName + "/" + Constants.DIR_FILE_NAME);
+		final String stepParentUUID;
+		if (Files.exists(cipherStepParentDirFile, LinkOption.NOFOLLOW_LINKS)) {
+			stepParentUUID = Files.readString(cipherStepParentDirFile, StandardCharsets.UTF_8);
+		} else {
+			Files.createDirectories(cipherStepParentDirFile.getParent());
+			stepParentUUID = UUID.randomUUID().toString();
+			Files.writeString(cipherStepParentDirFile, stepParentUUID, StandardCharsets.UTF_8, StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE);
+		}
+		String stepParentDirHash = cryptor.hashDirectoryId(stepParentUUID);
+		Path stepParentDir = dataDir.resolve(stepParentDirHash.substring(0, 2)).resolve(stepParentDirHash.substring(2)).toAbsolutePath();
+		Files.createDirectories(stepParentDir);
+		return new CryptoPathMapper.CiphertextDirectory(stepParentUUID, stepParentDir);
+	}
+
+	// visible for testing
+	void adoptOrphanedResource(Path oldCipherPath, String newClearname, CryptoPathMapper.CiphertextDirectory stepParentDir, FileNameCryptor cryptor, String longNameSuffix, MessageDigest sha1) throws IOException {
+		if (oldCipherPath.toString().endsWith(Constants.DEFLATED_FILE_SUFFIX)) {
+			var newCipherName = convertClearToCiphertext(cryptor, newClearname + longNameSuffix, stepParentDir.dirId);
+			var deflatedName = BaseEncoding.base64Url().encode(sha1.digest(newCipherName.getBytes(StandardCharsets.UTF_8))) + Constants.DEFLATED_FILE_SUFFIX;
+			Path targetPath = stepParentDir.path.resolve(deflatedName);
+			Files.move(oldCipherPath, targetPath);
+
+			//adjust name.c9s
+			try (var fc = Files.newByteChannel(targetPath.resolve(Constants.INFLATED_FILE_NAME), StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+				fc.write(ByteBuffer.wrap(newCipherName.getBytes(StandardCharsets.UTF_8)));
+			}
+		} else {
+			var newCipherName = convertClearToCiphertext(cryptor, newClearname, stepParentDir.dirId);
+			Path targetPath = stepParentDir.path.resolve(newCipherName);
+			Files.move(oldCipherPath, targetPath);
+		}
+	}
+
+	private static String createClearnameToBeShortened(int threshold) {
+		int neededLength = (threshold - 4) / 4 * 3 - 16;
+		return LONG_NAME_SUFFIX_BASE.repeat((neededLength % LONG_NAME_SUFFIX_BASE.length()) + 1);
+	}
+
+	private static String convertClearToCiphertext(FileNameCryptor cryptor, String clearTextName, String dirId) {
+		return cryptor.encryptFilename(BaseEncoding.base64Url(), clearTextName, dirId.getBytes(StandardCharsets.UTF_8)) + Constants.CRYPTOMATOR_FILE_SUFFIX;
+	}
+
+	private static CiphertextFileType determineCiphertextFileType(Path ciphertextPath) {
+		if (Files.exists(ciphertextPath.resolve(Constants.DIR_FILE_NAME), LinkOption.NOFOLLOW_LINKS)) {
+			return CiphertextFileType.DIRECTORY;
+		} else if (Files.exists(ciphertextPath.resolve(Constants.SYMLINK_FILE_NAME), LinkOption.NOFOLLOW_LINKS)) {
+			return CiphertextFileType.SYMLINK;
+		} else {
+			return CiphertextFileType.FILE;
+		}
+	}
+
+	private static MessageDigest getSha1MessageDigest() {
+		try {
+			return MessageDigest.getInstance("SHA1");
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalStateException("Every JVM needs to provide a SHA1 implementation.");
+		}
+	}
+
 }
---
+++ src/test/java/org/cryptomator/cryptofs/health/dirid/OrphanDirTest.java
@@ -0,0 +1,371 @@
+package org.cryptomator.cryptofs.health.dirid;
+
+import com.google.common.io.BaseEncoding;
+import org.cryptomator.cryptofs.CryptoPathMapper;
+import org.cryptomator.cryptofs.VaultConfig;
+import org.cryptomator.cryptofs.common.Constants;
+import org.cryptomator.cryptolib.api.Cryptor;
+import org.cryptomator.cryptolib.api.FileNameCryptor;
+import org.cryptomator.cryptolib.api.Masterkey;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.security.MessageDigest;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class OrphanDirTest {
+
+	@TempDir
+	public Path pathToVault;
+
+	private OrphanDir result;
+	private Path dataDir;
+	private Path cipherRoot;
+	private Path cipherRecovery;
+	private Path cipherOrphan;
+	private FileNameCryptor cryptor;
+
+	@BeforeEach
+	public void init() throws IOException {
+		Path p = Mockito.mock(Path.class, "ignored");
+		result = new OrphanDir(p);
+
+		dataDir = pathToVault.resolve("d");
+		cipherRoot = dataDir.resolve("00/0000");
+		cipherRecovery = dataDir.resolve("11/1111");
+		cipherOrphan = dataDir.resolve("33/3333");
+		Files.createDirectories(cipherRoot);
+		Files.createDirectories(cipherOrphan);
+
+		cryptor = Mockito.mock(FileNameCryptor.class);
+	}
+
+
+	@Nested
+	class PrepareRecoveryDirTests {
+
+		@BeforeEach
+		public void init() {
+			Mockito.doReturn("000000").when(cryptor).hashDirectoryId(Constants.ROOT_DIR_ID);
+			Mockito.doReturn("111111").when(cryptor).hashDirectoryId(Constants.RECOVERY_DIR_ID);
+			Mockito.doReturn("222222").when(cryptor).hashDirectoryId("aaaaaa");
+
+			Mockito.doReturn("1").when(cryptor).encryptFilename(BaseEncoding.base64Url(), Constants.RECOVERY_DIR_NAME, Constants.ROOT_DIR_ID.getBytes(StandardCharsets.UTF_8));
+		}
+
+
+		@Test
+		@DisplayName("prepareRecoveryDir() creates recovery dir if not existent")
+		public void testPrepareStepParentNonExistingRecoveryDir() throws IOException {
+			Path actualCipherDir = result.prepareRecoveryDir(pathToVault, cryptor);
+
+			Assertions.assertEquals(cipherRecovery, actualCipherDir);
+			Assertions.assertEquals(Constants.RECOVERY_DIR_ID, Files.readString(pathToVault.resolve("d/00/0000/1.c9r/dir.c9r"), StandardCharsets.UTF_8));
+			Assertions.assertTrue(Files.exists(cipherRecovery));
+		}
+
+
+		@Test
+		@DisplayName("prepareRecoveryDir() runs without error on existing recovery dir")
+		public void testPrepareStepParentExistingRecoveryDir() throws IOException {
+			Path existingRecoveryDirFile = cipherRoot.resolve("1.c9r/dir.c9r");
+			Files.createDirectories(existingRecoveryDirFile.getParent());
+			Files.writeString(existingRecoveryDirFile, Constants.RECOVERY_DIR_ID, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+			Files.createDirectories(cipherRecovery);
+
+			Path actualCipherDir = result.prepareRecoveryDir(pathToVault, cryptor);
+
+			Assertions.assertEquals(cipherRecovery, actualCipherDir);
+			Assertions.assertEquals(Constants.RECOVERY_DIR_ID, Files.readString(pathToVault.resolve("d/00/0000/1.c9r/dir.c9r"), StandardCharsets.UTF_8));
+			Assertions.assertTrue(Files.isDirectory(cipherRecovery));
+		}
+
+
+		@Test
+		@DisplayName("prepareRecoveryDir() simply integrates orphaned recovery dir")
+		public void testPrepareStepParentOrphanedRecoveryDir() throws IOException {
+			Path missingRecoveryDirFile = cipherRoot.resolve("1.c9r/dir.c9r");
+			Files.createDirectories(missingRecoveryDirFile.getParent());
+			Files.createDirectories(cipherRecovery);
+
+			Path actualCipherDir = result.prepareRecoveryDir(pathToVault, cryptor);
+
+			Assertions.assertEquals(cipherRecovery, actualCipherDir);
+			Assertions.assertEquals(Constants.RECOVERY_DIR_ID, Files.readString(pathToVault.resolve("d/00/0000/1.c9r/dir.c9r"), StandardCharsets.UTF_8));
+			Assertions.assertTrue(Files.isDirectory(cipherRecovery));
+		}
+
+
+		@Test
+		@DisplayName("prepareRecoveryDir() throws exception on existing recovery dir with wrong id")
+		public void testPrepareStepParentWithWrongRecoveryDir() throws IOException {
+			Path existingRecoveryDirFile = cipherRoot.resolve("1.c9r/dir.c9r");
+			Files.createDirectories(existingRecoveryDirFile.getParent());
+			Files.writeString(existingRecoveryDirFile, UUID.randomUUID().toString(), StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+
+			Assertions.assertThrows(FileAlreadyExistsException.class, () -> result.prepareRecoveryDir(pathToVault, cryptor));
+
+			Assertions.assertNotEquals(Constants.RECOVERY_DIR_ID, Files.readString(existingRecoveryDirFile, StandardCharsets.UTF_8));
+		}
+	}
+
+
+	@Nested
+	class PrepareStepParentTests {
+
+		private String clearStepParentName;
+
+		@BeforeEach
+		public void init() throws IOException {
+			clearStepParentName = "step-parent";
+			Files.createDirectories(cipherRecovery);
+
+			Mockito.doReturn("222222").when(cryptor).hashDirectoryId("aaaaaa");
+			Mockito.doReturn("1").when(cryptor).encryptFilename(BaseEncoding.base64Url(), Constants.RECOVERY_DIR_NAME, Constants.ROOT_DIR_ID.getBytes(StandardCharsets.UTF_8));
+			Mockito.doReturn("2").when(cryptor).encryptFilename(BaseEncoding.base64Url(), clearStepParentName, Constants.RECOVERY_DIR_ID.getBytes(StandardCharsets.UTF_8));
+		}
+
+		@Test
+		@DisplayName("prepareStepParent() runs without error on not-existing stepparent")
+		public void testPrepareStepParent() throws IOException {
+			try (var uuidClass = Mockito.mockStatic(UUID.class)) {
+				UUID uuid = Mockito.mock(UUID.class);
+				uuidClass.when(() -> UUID.randomUUID()).thenReturn(uuid);
+				Mockito.doReturn("aaaaaa").when(uuid).toString();
+
+				result.prepareStepParent(dataDir, cipherRecovery, cryptor, clearStepParentName);
+			}
+
+			Assertions.assertEquals("aaaaaa", Files.readString(cipherRecovery.resolve("2.c9r/dir.c9r"), StandardCharsets.UTF_8));
+			Assertions.assertTrue(Files.isDirectory(pathToVault.resolve("d/22/2222")));
+		}
+
+		@Test
+		@DisplayName("prepareStepParent() runs without error on existing stepparent")
+		public void testPrepareStepParentExistingStepParentDir() throws IOException {
+			Path existingStepparentDirFile = cipherRecovery.resolve("2.c9r/dir.c9r");
+			Files.createDirectories(existingStepparentDirFile.getParent());
+			Files.writeString(existingStepparentDirFile, "aaaaaa", StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+			Path cipherStepparent = dataDir.resolve("22/2222");
+			Files.createDirectories(cipherStepparent);
+
+			try (var uuidClass = Mockito.mockStatic(UUID.class)) {
+				UUID uuid = Mockito.mock(UUID.class);
+				uuidClass.when(() -> UUID.randomUUID()).thenReturn(uuid);
+				Mockito.doReturn("aaaaaa").when(uuid).toString();
+
+				result.prepareStepParent(dataDir, cipherRecovery, cryptor, clearStepParentName);
+			}
+
+			Assertions.assertEquals("aaaaaa", Files.readString(cipherRecovery.resolve("2.c9r/dir.c9r"), StandardCharsets.UTF_8));
+			Assertions.assertTrue(Files.isDirectory(pathToVault.resolve("d/22/2222")));
+		}
+
+
+		@Test
+		@DisplayName("prepareStepParent() runs without error on orphaned stepparent")
+		public void testPrepareStepParentOrphanedStepParentDir() throws IOException {
+			Path missingStepparentDirFile = cipherRecovery.resolve("2.c9r/dir.c9r");
+			Files.createDirectories(missingStepparentDirFile.getParent());
+			Path cipherStepparent = dataDir.resolve("22/2222");
+			Files.createDirectories(cipherStepparent);
+
+			try (var uuidClass = Mockito.mockStatic(UUID.class)) {
+				UUID uuid = Mockito.mock(UUID.class);
+				uuidClass.when(() -> UUID.randomUUID()).thenReturn(uuid);
+				Mockito.doReturn("aaaaaa").when(uuid).toString();
+
+				result.prepareStepParent(dataDir, cipherRecovery, cryptor, clearStepParentName);
+			}
+
+			Assertions.assertEquals("aaaaaa", Files.readString(cipherRecovery.resolve("2.c9r/dir.c9r"), StandardCharsets.UTF_8));
+			Assertions.assertTrue(Files.isDirectory(pathToVault.resolve("d/22/2222")));
+		}
+	}
+
+	@Nested
+	class AdoptOrphanedTests {
+
+		@Test
+		@DisplayName("adoptOrphanedResource runs for unshortened resource")
+		public void testAdoptOrphanedUnshortened() throws IOException {
+			String expectedMsg = "Please, sir, I want some more.";
+			Path oldCipherPath = cipherOrphan.resolve("orphan.c9r");
+			String newClearName = "OliverTwist";
+			Files.writeString(oldCipherPath, expectedMsg, StandardCharsets.UTF_8, StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE);
+
+			CryptoPathMapper.CiphertextDirectory stepParentDir = new CryptoPathMapper.CiphertextDirectory("aaaaaa", pathToVault.resolve("d/22/2222"));
+			Files.createDirectories(stepParentDir.path);
+
+			Mockito.doReturn("adopted").when(cryptor).encryptFilename(BaseEncoding.base64Url(), newClearName, stepParentDir.dirId.getBytes(StandardCharsets.UTF_8));
+			var sha1 = Mockito.mock(MessageDigest.class);
+
+			result.adoptOrphanedResource(oldCipherPath, newClearName, stepParentDir, cryptor, "longNameSuffix", sha1);
+
+			Assertions.assertEquals(expectedMsg, Files.readString(stepParentDir.path.resolve("adopted.c9r")));
+			Assertions.assertTrue(Files.notExists(oldCipherPath));
+		}
+
+
+		@Test
+		@DisplayName("adoptOrphanedResource runs for shortened resource with existing name.c9s")
+		public void testAdoptOrphanedShortened() throws IOException {
+			Path oldCipherPath = cipherOrphan.resolve("orphan.c9s");
+			String newClearName = "JimKnopf";
+			Files.createDirectories(oldCipherPath);
+			Files.createFile(oldCipherPath.resolve("name.c9s"));
+
+			CryptoPathMapper.CiphertextDirectory stepParentDir = new CryptoPathMapper.CiphertextDirectory("aaaaaa", pathToVault.resolve("d/22/2222"));
+			Files.createDirectories(stepParentDir.path);
+
+			Mockito.doReturn("adopted").when(cryptor).encryptFilename(Mockito.any(), Mockito.any(), Mockito.any());
+			try (var baseEncodingClass = Mockito.mockStatic(BaseEncoding.class)) {
+				MessageDigest sha1 = Mockito.mock(MessageDigest.class);
+				Mockito.doReturn(new byte[]{}).when(sha1).digest(Mockito.any());
+
+				BaseEncoding base64url = Mockito.mock(BaseEncoding.class);
+				baseEncodingClass.when(() -> BaseEncoding.base64Url()).thenReturn(base64url);
+				Mockito.doReturn("adopted_shortened").when(base64url).encode(Mockito.any());
+
+				result.adoptOrphanedResource(oldCipherPath, newClearName, stepParentDir, cryptor, "", sha1);
+			}
+
+			Assertions.assertTrue(Files.exists(stepParentDir.path.resolve("adopted_shortened.c9s")));
+			Assertions.assertEquals("adopted.c9r", Files.readString(stepParentDir.path.resolve("adopted_shortened.c9s/name.c9s")));
+			Assertions.assertTrue(Files.notExists(oldCipherPath));
+		}
+
+
+		@Test
+		@DisplayName("adoptOrphanedResource runs for shortened resource without existing name.c9s")
+		public void testAdoptOrphanedShortenedMissingNameC9s() throws IOException {
+			Path oldCipherPath = cipherOrphan.resolve("orphan.c9s");
+			String newClearName = "TomSawyer";
+			Files.createDirectories(oldCipherPath);
+
+			CryptoPathMapper.CiphertextDirectory stepParentDir = new CryptoPathMapper.CiphertextDirectory("aaaaaa", pathToVault.resolve("d/22/2222"));
+			Files.createDirectories(stepParentDir.path);
+
+			Mockito.doReturn("adopted").when(cryptor).encryptFilename(Mockito.any(), Mockito.any(), Mockito.any());
+			try (var baseEncodingClass = Mockito.mockStatic(BaseEncoding.class)) {
+				MessageDigest sha1 = Mockito.mock(MessageDigest.class);
+				Mockito.doReturn(new byte[]{}).when(sha1).digest(Mockito.any());
+
+				BaseEncoding base64url = Mockito.mock(BaseEncoding.class);
+				baseEncodingClass.when(() -> BaseEncoding.base64Url()).thenReturn(base64url);
+				Mockito.doReturn("adopted_shortened").when(base64url).encode(Mockito.any());
+
+				result.adoptOrphanedResource(oldCipherPath, newClearName, stepParentDir, cryptor, "", sha1);
+			}
+
+			Assertions.assertTrue(Files.exists(stepParentDir.path.resolve("adopted_shortened.c9s")));
+			Assertions.assertEquals("adopted.c9r", Files.readString(stepParentDir.path.resolve("adopted_shortened.c9s/name.c9s")));
+			Assertions.assertTrue(Files.notExists(oldCipherPath));
+		}
+
+	}
+
+
+	@Test
+	@DisplayName("fix() prepares vault, process every resource in orphanDir and deletes orphanDir")
+	public void testFix() throws IOException {
+		result = new OrphanDir(dataDir.relativize(cipherOrphan));
+		var resultSpy = Mockito.spy(result);
+
+		Path orphan1 = cipherOrphan.resolve("orphan1.c9r");
+		Path orphan2 = cipherOrphan.resolve("orphan2.c9s");
+		Files.createFile(orphan1);
+		Files.createDirectories(orphan2);
+
+		CryptoPathMapper.CiphertextDirectory stepParentDir = new CryptoPathMapper.CiphertextDirectory("aaaaaa", dataDir.resolve("22/2222"));
+
+		VaultConfig config = Mockito.mock(VaultConfig.class);
+		Mockito.doReturn(170).when(config).getShorteningThreshold();
+		Masterkey masterkey = Mockito.mock(Masterkey.class);
+		Cryptor generalCryptor = Mockito.mock(Cryptor.class);
+		Mockito.doReturn(cryptor).when(generalCryptor).fileNameCryptor();
+
+		Mockito.doReturn(cipherRecovery).when(resultSpy).prepareRecoveryDir(pathToVault, cryptor);
+		Mockito.doReturn(stepParentDir).when(resultSpy).prepareStepParent(Mockito.eq(dataDir), Mockito.eq(cipherRecovery), Mockito.eq(cryptor), Mockito.any());
+		Mockito.doAnswer(invocationOnMock -> {
+			Files.delete((Path) invocationOnMock.getArgument(0));
+			return null;
+		}).when(resultSpy).adoptOrphanedResource(Mockito.any(), Mockito.any(), Mockito.eq(stepParentDir), Mockito.eq(cryptor), Mockito.any(), Mockito.any());
+
+		resultSpy.fix(pathToVault, config, masterkey, generalCryptor);
+
+		Mockito.verify(resultSpy, Mockito.times(2)).adoptOrphanedResource(Mockito.any(), Mockito.any(), Mockito.eq(stepParentDir), Mockito.eq(cryptor), Mockito.any(), Mockito.any());
+		Assertions.assertTrue(Files.notExists(cipherOrphan));
+	}
+
+
+	@Test
+	@DisplayName("results with same orphan have write to same cleartext stepparent")
+	public void testFixRepeated() throws IOException {
+		VaultConfig config = Mockito.mock(VaultConfig.class);
+		Mockito.doReturn(170).when(config).getShorteningThreshold();
+		Masterkey masterkey = Mockito.mock(Masterkey.class);
+		Cryptor generalCryptor = Mockito.mock(Cryptor.class);
+		Mockito.doReturn(cryptor).when(generalCryptor).fileNameCryptor();
+
+		AtomicReference<String> clearStepparentNameRef = new AtomicReference<>("");
+
+		var interruptedResult = new OrphanDir(dataDir.relativize(cipherOrphan));
+		var interruptedSpy = Mockito.spy(interruptedResult);
+		Mockito.doReturn(cipherRecovery).when(interruptedSpy).prepareRecoveryDir(pathToVault, cryptor);
+		Mockito.doAnswer(invocation -> {
+			clearStepparentNameRef.set((String) invocation.getArgument(3));
+			throw new IOException("Interrupt");
+		}).when(interruptedSpy).prepareStepParent(Mockito.eq(dataDir), Mockito.eq(cipherRecovery), Mockito.eq(cryptor), Mockito.any());
+
+		var continuedResult = new OrphanDir(dataDir.relativize(cipherOrphan));
+		var continuedSpy = Mockito.spy(continuedResult);
+		Mockito.doReturn(cipherRecovery).when(continuedSpy).prepareRecoveryDir(pathToVault, cryptor);
+		Mockito.doThrow(IOException.class).when(continuedSpy).prepareStepParent(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
+
+		Assertions.assertThrows(IOException.class, () -> interruptedSpy.fix(pathToVault, config, masterkey, generalCryptor));
+		Assertions.assertThrows(IOException.class, () -> continuedSpy.fix(pathToVault, config, masterkey, generalCryptor));
+
+		Mockito.verify(continuedSpy).prepareStepParent(dataDir, cipherRecovery, cryptor, clearStepparentNameRef.get());
+	}
+
+
+	@Test
+	@DisplayName("orphaned recovery dir will only be reintegrated")
+	public void testFixOrphanedRecoveryDir() throws IOException {
+		Path orphanedRecovery = dataDir.resolve("11/1111");
+		result = new OrphanDir(dataDir.relativize(orphanedRecovery));
+		var resultSpy = Mockito.spy(result);
+
+		Path orphan1 = orphanedRecovery.resolve("orphan1.c9r");
+		Path orphan2 = orphanedRecovery.resolve("orphan2.c9r");
+		Files.createDirectories(orphan1);
+		Files.createDirectories(orphan2);
+
+
+		VaultConfig config = Mockito.mock(VaultConfig.class);
+		Mockito.doReturn(170).when(config).getShorteningThreshold();
+		Masterkey masterkey = Mockito.mock(Masterkey.class);
+		Cryptor generalCryptor = Mockito.mock(Cryptor.class);
+		Mockito.doReturn(cryptor).when(generalCryptor).fileNameCryptor();
+		Mockito.doReturn(cipherRecovery).when(resultSpy).prepareRecoveryDir(pathToVault, cryptor);
+
+		resultSpy.fix(pathToVault, config, masterkey, generalCryptor);
+
+		Mockito.verify(resultSpy, Mockito.never()).prepareStepParent(Mockito.eq(dataDir), Mockito.eq(cipherRecovery), Mockito.eq(cryptor), Mockito.any());
+		Mockito.verify(resultSpy, Mockito.never()).adoptOrphanedResource(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.eq(cryptor), Mockito.any(), Mockito.any());
+		Mockito.verify(resultSpy).prepareRecoveryDir(pathToVault, cryptor);
+	}
+}
