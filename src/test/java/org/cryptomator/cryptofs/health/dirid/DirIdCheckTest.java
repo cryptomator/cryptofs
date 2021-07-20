d41b4ac11bfdd44d42fd50982c2227bc482362db
Armin Schrenk <armin.schrenk@zoho.eu>
2021-07-20 10:47:43 +0200

    Health API / Directory Check: Result EmptyDirFile (#108)
    Add to DirIdCheck new result: EmptyDirId (including unit tests) 
    
    Co-authored-by: Sebastian Stenzel <overheadhunter@users.noreply.github.com>

--- src/test/java/org/cryptomator/cryptofs/health/dirid/DirIdCheckTest.java
+++ src/test/java/org/cryptomator/cryptofs/health/dirid/DirIdCheckTest.java
@@ -36,6 +36,7 @@ public class DirIdCheckTest {
 			AA/aaaa/baz=.c9r/symlink.c9r = linktarget
 			BB/bbbb/foo=.c9r/dir.c9r = CCcccc
 			BB/bbbb/bar=.c9r/dir.c9r = ffffffffffff-aaaaaaaaaaaa-tttttttttttt
+			BB/bbbb/baz=.c9r/dir.c9r = [EMPTY]
 			BB/bbbb/foo=.c9r/unrelated/dir.c9r = unrelatedfile
 			CC/cccc/foo=.c9r = file
 			""";
@@ -121,16 +122,32 @@ public class DirIdCheckTest {
 			MatcherAssert.assertThat(resultCaptor.getAllValues(), Matchers.hasItem(CustomMatchers.matching(ObeseDirFile.class, expectedObeseFile, "Obese dir file: /d/BB/bbbb/bar=.c9r/dir.c9r")));
 		}
 
+		@Test
+		@DisplayName("detects empty dirID file in /d/BB/bbbb/baz=.c9r/dir.c9r")
+		public void testVisitorDetectsEmptyDirId() throws IOException {
+			Files.walkFileTree(dataRoot, Set.of(), 4, visitor);
+
+			Predicate<EmptyDirFile> expectedEmptyFile = emptyDirFile -> "/d/BB/bbbb/baz=.c9r/dir.c9r".equals(emptyDirFile.dirFile.toString());
+			ArgumentCaptor<DiagnosticResult> resultCaptor = ArgumentCaptor.forClass(DiagnosticResult.class);
+			Mockito.verify(resultsCollector, Mockito.atLeastOnce()).accept(resultCaptor.capture());
+			MatcherAssert.assertThat(resultCaptor.getAllValues(), Matchers.hasItem(CustomMatchers.matching(EmptyDirFile.class, expectedEmptyFile, "Empty dir file: /d/BB/bbbb/baz=.c9r/dir.c9r")));
+		}
+
 	}
 
 	private static void initDirStructure(Path root, String line) throws UncheckedIOException {
 		try {
 			if (line.contains(" = ")) {
 				var sep = line.indexOf(" = ");
-				var file = line.substring(0, sep);
+				var file = root.resolve(line.substring(0, sep));
 				var contents = line.substring(sep + 3);
-				Files.createDirectories(root.resolve(file).getParent());
-				Files.writeString(root.resolve(file), contents, StandardCharsets.US_ASCII, StandardOpenOption.CREATE_NEW);
+				Files.createDirectories(file.getParent());
+				if (contents.equals("[EMPTY]")) {
+					Files.createFile(file);
+				} else {
+					Files.writeString(file, contents, StandardCharsets.US_ASCII, StandardOpenOption.CREATE_NEW);
+
+				}
 			} else {
 				Files.createDirectories(root.resolve(line));
 			}
