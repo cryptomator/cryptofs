3b52a4f5f6c67359383495cf6baced8f84cf828a
Armin Schrenk <armin.schrenk@skymatic.de>
2022-10-19 16:06:13 +0200

    remove unused method, add doc and unit test for util method

--- src/main/java/org/cryptomator/cryptofs/common/StringUtils.java
+++ src/main/java/org/cryptomator/cryptofs/common/StringUtils.java
@@ -5,23 +5,19 @@ package org.cryptomator.cryptofs.common;
  */
 public final class StringUtils {
 
+	/**
+	 * Removes the suffix of a string, if the string ends with the suffix.
+	 *
+	 * @param str input string
+	 * @param remove the suffix to match and remove
+	 * @return a copy of {@code str} with the suffix removed, otherwise just {@code str}
+	 */
 	public static String removeEnd(String str, String remove) {
 		if (str == null || remove == null) {
 			return str;
 		}
 		if (str.endsWith(remove)) {
 			return str.substring(0, str.length() - remove.length());
-		} else {
-			return str;
-		}
-	}
-
-	public static String removeStart(String str, String remove) {
-		if (str == null || remove == null) {
-			return str;
-		}
-		if (str.startsWith(remove)) {
-			return str.substring(remove.length());
 		} else {
 			return str;
 		}
