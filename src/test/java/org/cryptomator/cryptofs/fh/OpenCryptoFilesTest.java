62d7e9edf0c25d784983ce84a798c01369b84674
Armin Schrenk <armin.schrenk@skymatic.de>
2023-03-15 11:31:15 +0100

    Apply suggestions from code review
    Co-authored-by: Sebastian Stenzel <overheadhunter@users.noreply.github.com>

--- src/test/java/org/cryptomator/cryptofs/fh/OpenCryptoFilesTest.java
+++ src/test/java/org/cryptomator/cryptofs/fh/OpenCryptoFilesTest.java
@@ -16,7 +16,6 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 
 public class OpenCryptoFilesTest {
