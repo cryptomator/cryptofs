f79f84728e4c4f857895ef05d165a301d17c1aa7
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2023-04-12 09:29:56 +0200

    re-add ReadWriteLock

--- src/main/java/org/cryptomator/cryptofs/fh/ChunkCache.java
+++ src/main/java/org/cryptomator/cryptofs/fh/ChunkCache.java
@@ -13,6 +13,9 @@ import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.NonWritableChannelException;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @OpenFileScoped
 public class ChunkCache {
@@ -26,6 +29,23 @@ public class ChunkCache {
 	private final ExceptionsDuringWrite exceptionsDuringWrite;
 	private final Cache<Long, Chunk> chunkCache;
 	private final ConcurrentMap<Long, Chunk> cachedChunks;
+
+	/**
+	 * We have to deal with two forms of access to the {@link #chunkCache}:
+	 * <ol>
+	 *     <li>Accessing a single chunk (with a known index), e.g. during {@link #getChunk(long)}</li>
+	 *     <li>Accessing multiple chunks, e.g. during {@link #invalidateStale()}</li>
+	 * </ol>
+	 *
+	 * While the former can be handled by the cache implementation (based on {@link ConcurrentMap}) just fine,
+	 * we need to make sure no concurrent modification will happen accessing multiple chunks (e.g. when iterating over the entry set).
+	 *
+	 * This is achieved using this {@link ReadWriteLock}, where holding the {@link ReadWriteLock#readLock() shared lock} is
+	 * sufficient for index-based access, while the {@link ReadWriteLock#writeLock() exclusive lock} is necessary otherwise.
+	 */
+	private final ReadWriteLock lock = new ReentrantReadWriteLock();
+	private final Lock sharedLock = lock.readLock(); // required when accessing a single chunk
+	private final Lock exclusiveLock = lock.writeLock(); // required when accessing multiple chunks at once
 
 	@Inject
 	public ChunkCache(ChunkLoader chunkLoader, ChunkSaver chunkSaver, CryptoFileSystemStats stats, BufferPool bufferPool, ExceptionsDuringWrite exceptionsDuringWrite) {
@@ -59,18 +79,23 @@ public class ChunkCache {
 	 * @throws IllegalArgumentException If {@code chunkData}'s remaining bytes is not equal to the number of bytes fitting into a chunk
 	 */
 	public Chunk putChunk(long chunkIndex, ByteBuffer chunkData) throws IllegalArgumentException {
-		return cachedChunks.compute(chunkIndex, (index, chunk) -> {
-			if (chunk == null) {
-				chunk = new Chunk(chunkData, true, () -> releaseChunk(chunkIndex));
-			} else {
-				var dst = chunk.data().duplicate().clear();
-				Preconditions.checkArgument(chunkData.remaining() == dst.remaining());
-				dst.put(chunkData);
-				chunk.dirty().set(true);
-			}
-			chunk.currentAccesses().incrementAndGet();
-			return chunk;
-		});
+		sharedLock.lock();
+		try {
+			return cachedChunks.compute(chunkIndex, (index, chunk) -> {
+				if (chunk == null) {
+					chunk = new Chunk(chunkData, true, () -> releaseChunk(chunkIndex));
+				} else {
+					var dst = chunk.data().duplicate().clear();
+					Preconditions.checkArgument(chunkData.remaining() == dst.remaining());
+					dst.put(chunkData);
+					chunk.dirty().set(true);
+				}
+				chunk.currentAccesses().incrementAndGet();
+				return chunk;
+			});
+		} finally {
+			sharedLock.unlock();
+		}
 	}
 
 	/**
@@ -81,17 +106,22 @@ public class ChunkCache {
 	 * @throws IOException If reading or decrypting the chunk failed
 	 */
 	public Chunk getChunk(long chunkIndex) throws IOException {
-		stats.addChunkCacheAccess();
+		sharedLock.lock();
 		try {
-			return cachedChunks.compute(chunkIndex, (idx, chunk) -> {
-				if (chunk == null) {
-					chunk = loadChunk(idx);
-				}
-				chunk.currentAccesses().incrementAndGet();
-				return chunk;
-			});
-		} catch (UncheckedIOException | AuthenticationFailedException e) {
-			throw new IOException(e);
+			stats.addChunkCacheAccess();
+			try {
+				return cachedChunks.compute(chunkIndex, (idx, chunk) -> {
+					if (chunk == null) {
+						chunk = loadChunk(idx);
+					}
+					chunk.currentAccesses().incrementAndGet();
+					return chunk;
+				});
+			} catch (UncheckedIOException | AuthenticationFailedException e) {
+				throw new IOException(e);
+			}
+		} finally {
+			sharedLock.unlock();
 		}
 	}
 
@@ -106,10 +136,15 @@ public class ChunkCache {
 
 	@SuppressWarnings("resource")
 	private void releaseChunk(long chunkIndex) {
-		cachedChunks.computeIfPresent(chunkIndex, (idx, chunk) -> {
-			chunk.currentAccesses().decrementAndGet();
-			return chunk;
-		});
+		sharedLock.lock();
+		try {
+			cachedChunks.computeIfPresent(chunkIndex, (idx, chunk) -> {
+				chunk.currentAccesses().decrementAndGet();
+				return chunk;
+			});
+		} finally {
+			sharedLock.unlock();
+		}
 	}
 
 	/**
@@ -117,6 +152,7 @@ public class ChunkCache {
 	 * @see #invalidateStale()
 	 */
 	public void flush() throws IOException {
+		exclusiveLock.lock();
 		try {
 			cachedChunks.forEach((index, chunk) -> {
 				try {
@@ -127,6 +163,8 @@ public class ChunkCache {
 			});
 		} catch (UncheckedIOException e) {
 			throw new IOException(e);
+		} finally {
+			exclusiveLock.unlock();
 		}
 	}
 
@@ -134,9 +172,12 @@ public class ChunkCache {
 	 * Removes stale chunks from cache.
 	 */
 	public void invalidateStale() {
-		// This may not be atomic, however this method is only called during truncation.
-		// If chunks are added (and become stale) concurrently, behavior is undefined anyway
-		cachedChunks.entrySet().removeIf(entry -> entry.getValue().currentAccesses().get() == 0);
+		exclusiveLock.lock();
+		try {
+			cachedChunks.entrySet().removeIf(entry -> entry.getValue().currentAccesses().get() == 0);
+		} finally {
+			exclusiveLock.unlock();
+		}
 	}
 
 	// visible for testing
