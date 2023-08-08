6901c32e69775d212dae4e0c0144bef4215797a8
JaniruTEC <52893617+JaniruTEC@users.noreply.github.com>
2023-08-08 13:05:02 +0200

    Extracted InMemory-Tests to own class

--- src/test/java/org/cryptomator/cryptofs/CryptoFileSystemProviderIntegrationTest.java
+++ src/test/java/org/cryptomator/cryptofs/CryptoFileSystemProviderIntegrationTest.java
@@ -589,68 +589,6 @@ public class CryptoFileSystemProviderIntegrationTest {
 
 	}
 
-
-	@Nested
-	public class InMemory {
-
-		private static FileSystem tmpFs;
-		private static Path pathToVault;
-
-		@BeforeAll
-		public static void beforeAll() {
-			tmpFs = Jimfs.newFileSystem(Configuration.unix());
-			pathToVault = tmpFs.getPath("/vault");
-		}
-
-		@BeforeEach
-		public void beforeEach() throws IOException {
-			Files.createDirectory(pathToVault);
-		}
-
-		@AfterEach
-		public void afterEach() throws IOException {
-			try (var paths = Files.walk(pathToVault)) {
-				var nodes = paths.sorted(Comparator.reverseOrder()).toList();
-				for (var node : nodes) {
-					Files.delete(node);
-				}
-			}
-		}
-
-		@AfterAll
-		public static void afterAll() throws IOException {
-			tmpFs.close();
-		}
-
-		@Test
-		@DisplayName("Replace an existing, shortened file")
-		public void testReplaceExistingShortenedFile() throws IOException {
-			try (var fs = setupCryptoFs(50, 100, false)) {
-				var fiftyCharName2 = "/50char2_50char2_50char2_50char2_50char2_50char.txt"; //since filename encryption increases filename length, 50 cleartext chars are sufficient
-				var source = fs.getPath("/source.txt");
-				var target = fs.getPath(fiftyCharName2);
-				Files.createFile(source);
-				Files.createFile(target);
-
-				assertDoesNotThrow(() -> Files.move(source, target, REPLACE_EXISTING));
-				assertTrue(Files.notExists(source));
-				assertTrue(Files.exists(target));
-			}
-		}
-
-		private FileSystem setupCryptoFs(int ciphertextShorteningThreshold, int maxCleartextFilename, boolean readonly) throws IOException {
-			byte[] key = new byte[64];
-			Arrays.fill(key, (byte) 0x55);
-			var keyLoader = Mockito.mock(MasterkeyLoader.class);
-			Mockito.when(keyLoader.loadKey(Mockito.any())).thenAnswer(ignored -> new Masterkey(key));
-			var properties = CryptoFileSystemProperties.cryptoFileSystemProperties().withKeyLoader(keyLoader).withShorteningThreshold(ciphertextShorteningThreshold).withMaxCleartextNameLength(maxCleartextFilename).withFlags(readonly ? Set.of(CryptoFileSystemProperties.FileSystemFlags.READONLY) : Set.of()).build();
-			CryptoFileSystemProvider.initialize(pathToVault, properties, URI.create("test:key"));
-			URI fsUri = CryptoFileSystemUri.create(pathToVault);
-			return FileSystems.newFileSystem(fsUri, cryptoFileSystemProperties().withKeyLoader(keyLoader).build());
-		}
-
-	}
-
 	@Nested
 	@EnabledOnOs({OS.MAC, OS.LINUX})
 	@TestInstance(TestInstance.Lifecycle.PER_CLASS)
