2cd07fae1fc779bfa798ba287919ba9cb69227af
Armin Schrenk <armin.schrenk@skymatic.de>
2025-02-06 17:48:05 +0100

    standardize parameter naming for conflict resolution events

--- src/main/java/org/cryptomator/cryptofs/event/ConflictResolvedEvent.java
+++ src/main/java/org/cryptomator/cryptofs/event/ConflictResolvedEvent.java
@@ -3,13 +3,18 @@ package org.cryptomator.cryptofs.event;
 import java.nio.file.Path;
 
 /**
- * Emitted, if a conflict inside an encrypted directory was resolved
+ * Emitted, if a conflict inside an encrypted directory was resolved.
+ * <p>
+ * A conflict exists, if two encrypted files with the same base64url string exist, but the second file has an arbitrary suffix before the file extension.
+ * The file <i>without</i> the suffix is called <b>canonical</b>.
+ * The file <i>with the suffix</i> is called <b>conflicting</b>
+ * On successful conflict resolution the conflicting file is renamed to the <b>resolved</b> file
  *
- * @param cleartextPath path within the cryptographic filesystem
- * @param ciphertextPath path to the encrypted resource
- * @param oldVersionCleartextPath path within the cryptographic filesystem of the renamed resource
- * @param oldVersionCiphertextPath path to the renamed, encrypted resource
+ * @param canonicalCleartextPath path of the canonical file within the cryptographic filesystem
+ * @param conflictingCiphertextPath path of the encrypted, conflicting file
+ * @param resolvedCleartextPath path of the resolved file within the cryptographic filesystem
+ * @param resolvedCiphertextPath path of the resolved, encrypted file
  */
-public record ConflictResolvedEvent(Path cleartextPath, Path ciphertextPath, Path oldVersionCleartextPath, Path oldVersionCiphertextPath) implements FilesystemEvent {
+public record ConflictResolvedEvent(Path canonicalCleartextPath, Path conflictingCiphertextPath, Path resolvedCleartextPath, Path resolvedCiphertextPath) implements FilesystemEvent {
 
 }
