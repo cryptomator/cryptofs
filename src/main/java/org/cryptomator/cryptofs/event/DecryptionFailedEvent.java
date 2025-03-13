6a4ed6faf5e9932dc92bcab081fb96d8cd032b81
Armin Schrenk <armin.schrenk@skymatic.de>
2025-03-13 16:25:53 +0100

    Feature: Add timestamp to file system events (#291)

--- src/main/java/org/cryptomator/cryptofs/event/DecryptionFailedEvent.java
+++ src/main/java/org/cryptomator/cryptofs/event/DecryptionFailedEvent.java
@@ -1,12 +1,23 @@
 package org.cryptomator.cryptofs.event;
 
 import java.nio.file.Path;
+import java.time.Instant;
 
 /**
  * Emitted, if a decryption operation fails.
  *
+ * @param timestamp timestamp of event appearance
  * @param ciphertextPath path to the encrypted resource
  * @param e thrown exception
  */
-public record DecryptionFailedEvent(Path ciphertextPath, Exception e) implements FilesystemEvent {
+public record DecryptionFailedEvent(Instant timestamp, Path ciphertextPath, Exception e) implements FilesystemEvent {
+
+	public DecryptionFailedEvent(Path ciphertextPath, Exception e) {
+		this(Instant.now(), ciphertextPath, e);
+	}
+
+	@Override
+	public Instant getTimestamp() {
+		return timestamp;
+	}
 }
