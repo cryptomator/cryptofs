22cd7c93ebbf57b2fa6d316a3d420c91786d519f
Sebastian Stenzel <overheadhunter@users.noreply.github.com>
2025-08-09 14:05:22 +0200

    Update src/test/java/org/cryptomator/cryptofs/attr/CryptoDosFileAttributesTest.java

--- src/test/java/org/cryptomator/cryptofs/attr/CryptoDosFileAttributesTest.java
+++ src/test/java/org/cryptomator/cryptofs/attr/CryptoDosFileAttributesTest.java
@@ -119,9 +119,9 @@ public class CryptoDosFileAttributesTest {
 			when(delegate.isReadOnly()).thenReturn(value);
 			var inTest = new CryptoDosFileAttributes(delegate, FILE, path, cryptor, Optional.of(openCryptoFile), cryptoFileSystemProperties);
 
-			verify(delegate, times(1)).isReadOnly();
+			verify(delegate, Mockito.atMostOnce()).isReadOnly();
 			Assertions.assertTrue(inTest.isReadOnly());
-			verify(delegate, times(1)).isReadOnly();
+			verify(delegate, Mockito.atMostOnce()).isReadOnly();
 		}
 	}
 }
