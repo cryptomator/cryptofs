dcd6dabd0a21233be3e1cc7796c6742e463445ae
Armin Schrenk <armin.schrenk@skymatic.de>
2025-10-10 13:22:18 +0200

    add documentation to InUseManager interface

--- src/main/java/org/cryptomator/cryptofs/inuse/InUseManager.java
+++ src/main/java/org/cryptomator/cryptofs/inuse/InUseManager.java
@@ -4,8 +4,22 @@ import java.nio.file.Path;
 import java.time.Instant;
 import java.util.Optional;
 
+/**
+ * A file is considered <it>in use</it>, if:
+ * <ul>
+ *     <li> an in-use-file for the given file exists</li>
+ *     <li> that in-use-file belongs to the running crypto filesystem</li>
+ *     <li> the last update time is at most X minutes ago</li>
+ * </ul>.
+ */
 public interface InUseManager {
 
+	/**
+	 * Checks if the given ciphertext path is used by others.
+	 *
+	 * @param ciphertextPath Path to the ciphertext file for which the usage is checked
+	 * @return {@code true} if the file is <it>used</it> by others. {@code false} otherwise
+	 */
 	default boolean isInUseByOthers(Path ciphertextPath) {
 		return false;
 	}
@@ -21,6 +35,13 @@ public interface InUseManager {
 	}
 
 
+	/**
+	 * Marks the given ciphertextpath as <it>used</it>.
+	 *
+	 * @param ciphertextPath Path to the ciphertext file which should be marked as used.
+	 * @return A {@link UseToken} representing a use-ship for this file
+	 * @throws FileAlreadyInUseException if the file is already in use by a different owner
+	 */
 	default UseToken use(Path ciphertextPath) throws FileAlreadyInUseException {
 		return UseToken.INIT_TOKEN;
 	}
