2bccc67cba462e6013d183c92a9c05a83a3bfb88
Armin Schrenk <armin.schrenk@skymatic.de>
2026-08-27 16:36:46 +0200

    fix wrong import

--- src/main/java/org/cryptomator/cryptofs/CryptoFileSystemModule.java
+++ src/main/java/org/cryptomator/cryptofs/CryptoFileSystemModule.java
@@ -19,7 +19,7 @@ import org.cryptomator.cryptolib.api.Cryptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Named;
+import jakarta.inject.Named;
 import java.io.IOException;
 import java.nio.file.FileStore;
 import java.nio.file.Files;
